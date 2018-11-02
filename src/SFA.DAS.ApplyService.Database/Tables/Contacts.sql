b6e72fece239deab3594e2473aa7ba3d07dac46b
Alan Burns <adburns57@gmail.com>
2018-11-02 11:54:16 +0000

    reinstated FamilyName and GivenNames (as already used!)

--- src/SFA.DAS.ApplyService.Database/Tables/Contacts.sql
+++ src/SFA.DAS.ApplyService.Database/Tables/Contacts.sql
@@ -2,10 +2,12 @@
 CREATE TABLE [dbo].[Contacts](
 	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
    Email	[nvarchar](120)	NOT NULL,	-- Email Address used to uniquely identify Applicant
+   [GivenNames] [nvarchar](250)  NOT NULL, 
+   [FamilyName] [nvarchar](250)  NOT NULL, 
+   [SigninId] [uniqueidentifier] NULL,-- when Known SigninID (DFE or Provider Idams Username)
+   [SigninType] [nvarchar](20)  NULL, -- when Known 'DfESignin','ProvideridAMS'
    ApplyOrganisationID [uniqueidentifier] NULL, -- When Known organisation for the apply contact
-   ContactDetails [nvarchar](max)	 NULL, --  Contact Details ("GivenNames","FamilyName",
-   SigninId [uniqueidentifier] NULL,-- when Known SigninID (DFE or Provider Idams Username)
-   SigninType [nvarchar](20)  NULL, -- when Known 'DFE Signin','Provider idAMS'
+   ContactDetails [nvarchar](max)	 NULL, --  Contact Details 
    Status	[nvarchar](	20)	NOT NULL,	-- 'new', 'inprogress' – signup awaiting callback from DFE Signin, 'live' is live , 'deleted' is no longer to be used
    IsApproved [bit] NOT NULL DEFAULT 0, -- set when this user is approved to represent the Organisation
    CreatedAt	Datetime2(7)	NOT NULL,	--Date / Time that the record was created
