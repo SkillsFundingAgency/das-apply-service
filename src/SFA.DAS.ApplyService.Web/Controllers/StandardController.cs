7c508bb076acc1de6142be9494343243034bc823
MarkFCain <mark.cain@digital.education.gov.uk>
2019-01-07 11:24:36 +0000

    ON-1140 add search results page and confirmation page

--- src/SFA.DAS.ApplyService.Web/Controllers/StandardController.cs
+++ src/SFA.DAS.ApplyService.Web/Controllers/StandardController.cs
@@ -13,11 +13,13 @@ namespace SFA.DAS.ApplyService.Web.Controllers
 {
     public class StandardController : Controller
     {
+        private readonly AssessorServiceApiClient _assessorServiceApiClient;
         private readonly IApplicationApiClient _apiClient;
 
-        public StandardController(IApplicationApiClient apiClient)
+        public StandardController(IApplicationApiClient apiClient, AssessorServiceApiClient assessorServiceApiClient)
         {
             _apiClient = apiClient;
+            _assessorServiceApiClient = assessorServiceApiClient;
         }
 
         [HttpGet("Standard/{applicationId}")]
@@ -27,34 +29,56 @@ namespace SFA.DAS.ApplyService.Web.Controllers
             return View("~/Views/Application/Standard/FindStandard.cshtml", standardViewModel);
         }
 
+        //[HttpPost("Standard/{applicationId}")]
+        //public async Task<IActionResult> Search(Guid applicationId, string standardToFind, StandardViewModel model)
+        //{
+        //    // TODO: Check standard is valid.
+
+        //    var standardApplicationData = new StandardApplicationData {StandardName = standardToFind};
+
+        //    //await _apiClient.UpdateApplicationData(standardApplicationData, applicationId);
+
+        //    ////var sequence = _apiClient.GetSequence(applicationId, Guid.Parse(User.FindFirstValue("UserId")));
+
+        //    //return RedirectToAction("Sequence", "Application", new {applicationId});
+
+        //    return View("~/Views/Application/Standard/FindStandard.cshtml", applicationId);
+        //}
+
         [HttpPost("Standard/{applicationId}")]
-        public async Task<IActionResult> Search(Guid applicationId, string standardToFind, StandardViewModel model)
+        public async Task<IActionResult> Search(StandardViewModel model)
         {
-            // TODO: Check standard is valid.
+            //            var results = new[]
+            //            {
+            //                new {StandardName = "Able Seafarer", PreRequesites = "<b>Some pre-reqs</b><ul><li>Must do this...</li></ul>"},
+            //                new {StandardName = "Lion tamer", PreRequesites = "<b>Different pre-reqs</b><ul><li>Maybe this...</li><li>And this...</li></ul>"},
+            //            };
 
-            var standardApplicationData = new StandardApplicationData {StandardName = standardToFind};
+        if (string.IsNullOrEmpty(model.StandardToFind) || model.StandardToFind.Length < 2)
+            {
+                ModelState.AddModelError(nameof(model.StandardToFind), "Enter a valid search string (more than 2 characters)");
+                TempData["ShowErrors"] = true;
+                return RedirectToAction(nameof(Index));
+            }
 
-            await _apiClient.UpdateApplicationData(standardApplicationData, applicationId);
+            var results = await _assessorServiceApiClient.GetStandards();
 
-            //var sequence = _apiClient.GetSequence(applicationId, Guid.Parse(User.FindFirstValue("UserId")));
+            model.Results = results.Where(r => r.Title.ToLower().Contains(model.StandardToFind.ToLower())).ToList();
 
-            return RedirectToAction("Sequence", "Application", new {applicationId});
+            return View("~/Views/Application/Standard/FindStandardResults.cshtml", model);
 
-            //return View("~/Views/Application/Standard/FindStandard.cshtml", applicationId);
+
         }
 
-        [HttpGet("Standard/Search/{search}")]
-        public async Task<ActionResult> Search(string search)
-        {
-            var results = new[]
-            {
-                new {StandardName = "Able Seafarer", PreRequesites = "<b>Some pre-reqs</b><ul><li>Must do this...</li></ul>"},
-                new {StandardName = "Lion tamer", PreRequesites = "<b>Different pre-reqs</b><ul><li>Maybe this...</li><li>And this...</li></ul>"},
-            };
 
-            results = results.Where(r => r.StandardName.ToLower().Contains(search.ToLower())).ToArray();
-            
-            return Ok(results);
+        [HttpGet("Standard/confirm-standard/{applicationId}/standard/{standardCode}")]
+        public async Task<IActionResult> Index(Guid applicationId, int standardCode)
+        {
+            var standardViewModel = new StandardViewModel { ApplicationId = applicationId, StandardCode = standardCode};
+            var results = await _assessorServiceApiClient.GetStandards();
+            standardViewModel.SelectedStandard = results.FirstOrDefault(r => r.StandardId == standardCode);
+
+            return View("~/Views/Application/Standard/ConfirmStandard.cshtml", standardViewModel);
         }
     }
 }
--- src/SFA.DAS.ApplyService.Web/ViewModels/StandardViewModel.cs
+++ src/SFA.DAS.ApplyService.Web/ViewModels/StandardViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.ApplyService.InternalApi.Models.AssessorService;
 
 namespace SFA.DAS.ApplyService.Web.ViewModels
 {
@@ -6,8 +8,12 @@ namespace SFA.DAS.ApplyService.Web.ViewModels
     {
         public Guid ApplicationId { get; set; }
 
-        public string StandardName { get; set; }
+        public string StandardToFind { get; set; }
 
         public int StandardCode { get; set; }
+
+        public List<StandardCollation> Results { get; set; }
+
+        public StandardCollation SelectedStandard { get; set; }
     }
 }
