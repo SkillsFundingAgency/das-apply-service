a805b68d56299f194a1c13ca8b61826063ff898b
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-01-18 13:28:36 +0000

    ON-1388 - Delete Page Feedback

--- src/SFA.DAS.ApplyService.Domain/Apply/Feedback.cs
+++ src/SFA.DAS.ApplyService.Domain/Apply/Feedback.cs
@@ -4,6 +4,7 @@ namespace SFA.DAS.ApplyService.Domain.Apply
 {
     public class Feedback
     {
+        public Guid Id { get; set; }
         public string From { get; set; }
         public string Message { get; set; }
         public DateTime Date { get; set; }
