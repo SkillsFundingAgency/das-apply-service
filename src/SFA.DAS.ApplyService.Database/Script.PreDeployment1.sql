7b4794e4e66de3517019bc0bba6a9d97f0f4cdb9
Alan Burns <adburns57@gmail.com>
2019-01-22 11:18:01 +0000

    remove predeployment script

--- src/SFA.DAS.ApplyService.Database/Script.PreDeployment1.sql
+++
@@ -1,3 +0,0 @@
-﻿IF EXISTS(SELECT * FROM sys.objects o INNER JOIN sys.columns c ON o.object_id = c.object_id WHERE o.name = 'ApplicationSequences' AND c.name = 'SequenceData')
-ALTER TABLE dbo.ApplicationSequences DROP COLUMN SequenceData
-GO
