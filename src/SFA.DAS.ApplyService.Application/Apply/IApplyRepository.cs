dfec1cb371afd25f1aa21adbd099787fcf6d97e6
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-02-08 09:02:43 +0000

    ON-1447 - Fixed merge issues

--- src/SFA.DAS.ApplyService.Application/Apply/IApplyRepository.cs
+++ src/SFA.DAS.ApplyService.Application/Apply/IApplyRepository.cs
@@ -22,10 +22,6 @@ namespace SFA.DAS.ApplyService.Application.Apply
         Task UpdateSections(List<ApplicationSection> sections);
         Task UpdateSequences(List<ApplicationSequence> sequences);
         Task SaveSection(ApplicationSection section, Guid? userId = null);
-        Task<Guid> CreateNewWorkflow(string workflowType);
-        Task CreateSequence(Guid workflowId, double sequenceId, bool isActive);
-        Task CreateSection(WorkflowSection section);
-        Task AddAssets(Dictionary<string,string> assets);
         Task<List<ApplicationSummaryItem>> GetOpenApplications(int sequenceId);
         Task<List<ApplicationSummaryItem>> GetFeedbackAddedApplications();
         Task<List<ApplicationSummaryItem>> GetClosedApplications();
