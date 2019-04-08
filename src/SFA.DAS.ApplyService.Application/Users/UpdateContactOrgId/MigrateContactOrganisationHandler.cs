25e227b4b634de0f6b98809c1c9ceee76863b564
ArshedMust <arshed@air2wire.com>
2019-04-08 20:49:18 +0100

    Updated the migration of assessor users and orgs so that individual orgs and contacts can be created from assessor

--- src/SFA.DAS.ApplyService.Application/Users/UpdateContactOrgId/MigrateContactOrganisationHandler.cs
+++ src/SFA.DAS.ApplyService.Application/Users/UpdateContactOrgId/MigrateContactOrganisationHandler.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.ApplyService.Application.Organisations;
+using SFA.DAS.ApplyService.Domain.Entities;
 
 namespace SFA.DAS.ApplyService.Application.Users.UpdateContactOrgId
 {
@@ -24,45 +25,49 @@ namespace SFA.DAS.ApplyService.Application.Users.UpdateContactOrgId
             var newOrganisationCreated = false;
             var requestContact = request.Contact;
             var requestOrganisation = request.Organisation;
-            if (requestContact != null && requestOrganisation != null)
+            if (requestContact != null )
             {
                 var organisationId = Guid.Empty;
-                var organisation = await _organisationRepository.GetOrganisationByName(requestOrganisation.Name) ?? 
-                                   (await _organisationRepository.GetOrganisationByName(requestOrganisation.OrganisationDetails.LegalName) ??
-                                    await _organisationRepository.GetOrganisationByName(requestOrganisation.OrganisationDetails.ProviderName));
-
-                if (organisation != null)
+                if (requestOrganisation != null)
                 {
-                    //Update organisation details
-                    organisation.OrganisationDetails = requestOrganisation.OrganisationDetails;
-                    organisation= await  _organisationRepository.UpdateOrganisation(organisation);
-                    organisationId = organisation.Id;
-                }
-                else
-                {
-                    //Create new organisation
-                    organisation= await _organisationRepository.CreateOrganisation(requestOrganisation);
-                    organisationId = organisation.Id;
-                    newOrganisationCreated = true;
-                }
+                    var organisation = await _organisationRepository.GetOrganisationByName(requestOrganisation.Name) ??
+                                       (await _organisationRepository.GetOrganisationByName(requestOrganisation.OrganisationDetails.LegalName) ??
+                                        await _organisationRepository.GetOrganisationByName(requestOrganisation.OrganisationDetails.ProviderName));
 
+                    if (organisation != null)
+                    {
+                        //Update organisation details
+                        organisation.OrganisationDetails = requestOrganisation.OrganisationDetails;
+                        organisation = await _organisationRepository.UpdateOrganisation(organisation);
+                        organisationId = organisation.Id;
+                    }
+                    else
+                    {
+                        //Create new organisation
+                        organisation = await _organisationRepository.CreateOrganisation(requestOrganisation);
+                        organisationId = organisation.Id;
+                        newOrganisationCreated = true;
+                    }
+                }
                 var signinId = requestContact.SigninId ?? Guid.Empty;
                 var contact = await _contactRepository.GetContactBySignInId(signinId);
                 if (contact != null)
                 {
                     //Update organisation association
-                    if(contact.ApplyOrganisationId == null)
+                    if(contact.ApplyOrganisationId == null && organisationId != Guid.Empty)
                         await _contactRepository.UpdateApplyOrganisationId(requestContact.Id, organisationId);
                 }
                 else
                 {
-                    if (organisationId != Guid.Empty)
-                    {
-                        //Create new contact
-                       var newContact= await _contactRepository.CreateContact(requestContact, organisationId);
-                       if(newOrganisationCreated)
-                            await _organisationRepository.UpdateOrganisation(organisationId, newContact.Id);
-                    }
+                    //Create new contact 
+                    Contact newContact = null;
+                    if (organisationId == Guid.Empty)
+                        newContact = await _contactRepository.CreateContact(requestContact,null);
+                    else
+                        newContact = await _contactRepository.CreateContact(requestContact, organisationId);
+
+                    if (newOrganisationCreated)
+                        await _organisationRepository.UpdateOrganisation(organisationId, newContact.Id);
                 }
             }
 
