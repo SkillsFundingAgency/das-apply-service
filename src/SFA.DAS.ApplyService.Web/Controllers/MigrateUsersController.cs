e7c0b2bdc7570f71bb6dabac6d21476575172047
davidgouge <davegouge@googlemail.com>
2019-05-01 09:22:08 +0100

    Reverted change to web endpoint being called

---
+++ src/SFA.DAS.ApplyService.Web/Controllers/MigrateUsersController.cs
@@ -0,0 +1,35 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApplyService.Domain.Entities;
+using SFA.DAS.ApplyService.InternalApi.Types;
+using SFA.DAS.ApplyService.Web.Infrastructure;
+
+namespace SFA.DAS.ApplyService.Web.Controllers
+{
+    public class MigrateUsersController : Controller
+    {
+        private readonly IUsersApiClient _usersApiClient;
+
+        public MigrateUsersController(IUsersApiClient usersApiClient)
+        {
+            _usersApiClient = usersApiClient;
+        }
+
+        [HttpPost("/MigrateUsers")]
+        public async Task<IActionResult> MigrateUsers()
+        {
+            await _usersApiClient.MigrateUsers();
+            return Ok();
+        }
+
+
+        [HttpPost("/MigrateContactAndOrgs")]
+        public async Task<IActionResult> MigrateContactAndOrgs([FromBody]MigrateContactOrganisation migrateContactOrganisation)
+        {
+            await _usersApiClient.MigrateContactAndOrgs(migrateContactOrganisation);
+            return Ok();
+        }
+
+       
+    }
+}
