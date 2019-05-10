1b732b7e148ca94b67b0c9d54287a6f0685f2720
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-05-10 14:50:22 +0100

    ON-1737 - Add Name to User Identity

--- src/SFA.DAS.ApplyService.Web/Infrastructure/AuthorizationStartupExtensions.cs
+++ src/SFA.DAS.ApplyService.Web/Infrastructure/AuthorizationStartupExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
@@ -83,6 +84,13 @@ namespace SFA.DAS.ApplyService.Web.Infrastructure
                             var user = await client.GetUserBySignInId(signInId);
                             if (user != null)
                             {
+                                var primaryIdentity = context.Principal.Identities.FirstOrDefault();
+                                if (primaryIdentity != null && string.IsNullOrEmpty(primaryIdentity.Name))
+                                {
+                                    primaryIdentity.AddClaim(new Claim(ClaimTypes.Name, $"{user.GivenNames} {user.FamilyName}"));
+                                    // Note: In future, may want to consider populating the other Claims, such as Email
+                                }
+
                                 var identity = new ClaimsIdentity(new List<Claim>(){new Claim("UserId", user.Id.ToString())});                      
                                 context.Principal.AddIdentity(identity);   
                             }
