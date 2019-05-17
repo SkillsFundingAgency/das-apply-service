6a63b03ee97b77012e27c5e9df21d872cc8f10ba
Dave Gouge <davegouge@gmail.com>
2019-05-17 11:28:23 +0100

    Extra logging

--- src/SFA.DAS.ApplyService.Web/Infrastructure/TokenService.cs
+++ src/SFA.DAS.ApplyService.Web/Infrastructure/TokenService.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using SFA.DAS.ApplyService.Configuration;
 
@@ -8,10 +9,13 @@ namespace SFA.DAS.ApplyService.Web.Infrastructure
     {
         private readonly IApplyConfig _configuration;
         private readonly IHostingEnvironment _hostingEnvironment;
-        public TokenService(IApplyConfig configuration, IHostingEnvironment hostingEnvironment)
+        private readonly ILogger<TokenService> _logger;
+
+        public TokenService(IApplyConfig configuration, IHostingEnvironment hostingEnvironment, ILogger<TokenService> logger)
         {
             _configuration = configuration;
             _hostingEnvironment = hostingEnvironment;
+            _logger = logger;
         }
 
         public string GetToken()
@@ -19,6 +23,26 @@ namespace SFA.DAS.ApplyService.Web.Infrastructure
             if (_hostingEnvironment.IsDevelopment())
                 return string.Empty;
 
+            _logger.LogInformation("Inside GetToken.");
+            if (_configuration == null)
+            {
+                _logger.LogInformation("_configuration is null");    
+            }
+            else
+            {
+                _logger.LogInformation("_configuration is not null");
+            }
+
+            if (_configuration.InternalApi == null)
+            {
+                _logger.LogInformation("_configuration.InternalApi is null");
+            }
+            
+            if (_configuration.InternalApi == null)
+            {
+                _logger.LogInformation("_configuration.InternalApi is null");
+            }
+            
             var tenantId = _configuration.InternalApi.TenantId;
             var clientId = _configuration.InternalApi.ClientId;
             var appKey = _configuration.InternalApi.ClientSecret;
