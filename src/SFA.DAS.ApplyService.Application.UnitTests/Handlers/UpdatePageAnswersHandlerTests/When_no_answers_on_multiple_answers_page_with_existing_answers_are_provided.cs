9934be25fa93827a74edf652eed24082a987c824
Martin Hornsby <martin-hornsby@ntlworld.com>
2019-05-21 11:22:20 +0100

    ON-1793 - Reverted changes due to requirements change

--- src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/UpdatePageAnswersHandlerTests/When_no_answers_on_multiple_answers_page_with_existing_answers_are_provided.cs
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/UpdatePageAnswersHandlerTests/When_no_answers_on_multiple_answers_page_with_existing_answers_are_provided.cs
@@ -49,7 +49,7 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.UpdatePageAnswersH
         }
 
         [Test]
-        public void Then_section_is_not_saved()
+        public void Then_section_is_not_saved_with_the_new_answers()
         {
             Handler.Handle(new UpdatePageAnswersRequest(ApplicationId, UserId, 1, 1, "3", 
                 new List<Answer>()
@@ -70,11 +70,32 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.UpdatePageAnswersH
                     section.QnAData.Pages.First(p => p.PageId == "3").PageOfAnswers[0].Answers[1].Value == AnswerQ5.Value), UserId), Times.Never());
         }
 
+        [Test]
+        public void Then_section_is_saved_with_the_existing_answers()
+        {
+            Handler.Handle(new UpdatePageAnswersRequest(ApplicationId, UserId, 1, 1, "3",
+                new List<Answer>()
+                {
+                    AnswerQ4,
+                    AnswerQ5
+                },
+                false), new CancellationToken()).Wait();
+
+            ApplyRepository.Verify(r => r.SaveSection(It.Is<ApplicationSection>(
+                section =>
+                    section.QnAData.Pages.First(p => p.PageId == "3").PageOfAnswers[0].Answers[0].QuestionId == _existingAnswerQ4.QuestionId &&
+                    section.QnAData.Pages.First(p => p.PageId == "3").PageOfAnswers[0].Answers[0].Value == _existingAnswerQ4.Value), UserId), Times.Once());
+
+            ApplyRepository.Verify(r => r.SaveSection(It.Is<ApplicationSection>(
+                section =>
+                    section.QnAData.Pages.First(p => p.PageId == "3").PageOfAnswers[0].Answers[1].QuestionId == _existingAnswerQ5.QuestionId &&
+                    section.QnAData.Pages.First(p => p.PageId == "3").PageOfAnswers[0].Answers[1].Value == _existingAnswerQ5.Value), UserId), Times.Once());
+        }
 
         [Test]
-        public void Then_page_is_returned_with_validation_errors()
+        public void Then_page_is_returned_with_correct_next_page()
         {
-            var result = Handler.Handle(new UpdatePageAnswersRequest(ApplicationId, UserId, 1, 1, "3", 
+            var result = Handler.Handle(new UpdatePageAnswersRequest(ApplicationId, UserId, 1, 1, "3",
                 new List<Answer>()
                 {
                     AnswerQ4,
@@ -82,9 +103,12 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.UpdatePageAnswersH
                 },
                 false), new CancellationToken()).Result;
 
-            result.Page.Next[0].ConditionMet.Should().Be(false);
-            result.ValidationErrors.Should().NotBeEmpty();
-            result.ValidationPassed.Should().BeFalse();
+            result.Page.Next.Should().ContainSingle(p => p.ConditionMet);
+            result.Page.Next[0].Action.Should().Be("NextPage");
+            result.Page.Next[0].ReturnId.Should().Be("4");
+
+            result.ValidationErrors.Should().BeNull();
+            result.ValidationPassed.Should().BeTrue();
         }
 
         [Test]
