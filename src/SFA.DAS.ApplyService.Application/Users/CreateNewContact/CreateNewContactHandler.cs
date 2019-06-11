25c845f6c2e79ba364d960e8b4d3c9b940a302e8
Dave Gouge <davegouge@gmail.com>
2019-06-11 07:43:09 +0100

    Change to create contact from assessor to only create if org exists

--- src/SFA.DAS.ApplyService.Application/Users/CreateNewContact/CreateNewContactHandler.cs
+++ src/SFA.DAS.ApplyService.Application/Users/CreateNewContact/CreateNewContactHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using SFA.DAS.ApplyService.Application.Organisations;
 using SFA.DAS.ApplyService.Domain.Entities;
 
 namespace SFA.DAS.ApplyService.Application.Users.CreateNewContact
@@ -9,26 +10,33 @@ namespace SFA.DAS.ApplyService.Application.Users.CreateNewContact
     public class CreateNewContactHandler : IRequestHandler<CreateNewContactRequest>
     {
         private readonly IContactRepository _contactRepository;
+        private readonly IOrganisationRepository _organisationRepository;
 
-        public CreateNewContactHandler(IContactRepository contactRepository)
+        public CreateNewContactHandler(IContactRepository contactRepository, IOrganisationRepository organisationRepository)
         {
             _contactRepository = contactRepository;
+            _organisationRepository = organisationRepository;
         }
         
         public async Task<Unit> Handle(CreateNewContactRequest request, CancellationToken cancellationToken)
         {
-            await _contactRepository.CreateContact(new Contact
+            var existingOrganisation = await _organisationRepository.GetOrganisation(request.OrganisationId);
+
+            if (existingOrganisation != null)
             {
-                Id = request.Id,
-                CreatedAt = DateTime.UtcNow, 
-                CreatedBy = "Invitation", 
-                Email = request.Email,
-                FamilyName = request.FamilyName, 
-                GivenNames = request.GivenNames,
-                IsApproved = true,
-                SigninType = request.SignInType, 
-                Status = request.Status
-            }, request.OrganisationId);
+                await _contactRepository.CreateContact(new Contact
+                {
+                    Id = request.Id,
+                    CreatedAt = DateTime.UtcNow, 
+                    CreatedBy = "Invitation", 
+                    Email = request.Email,
+                    FamilyName = request.FamilyName, 
+                    GivenNames = request.GivenNames,
+                    IsApproved = true,
+                    SigninType = request.SignInType, 
+                    Status = request.Status
+                }, request.OrganisationId);
+            }
             
             return Unit.Value;
         }
