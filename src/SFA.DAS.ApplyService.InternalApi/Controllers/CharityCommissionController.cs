de23d34821f3c55df765209120b28a328c670494
Dave Read <david.read@digital.education.gov.uk>
2019-06-24 11:42:43 +0100

    APR-519 add Polly retry policy for Companies House and Charity Commission APIs

--- src/SFA.DAS.ApplyService.InternalApi/Controllers/CharityCommissionController.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Controllers/CharityCommissionController.cs
@@ -4,6 +4,9 @@
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
     using System.Threading.Tasks;
+    using Polly;
+    using Polly.Retry;
+    using System;
 
     public class CharityCommissionController : Controller
     {
@@ -11,16 +14,19 @@
 
         private CharityCommissionApiClient _apiClient;
 
+        private AsyncRetryPolicy _retryPolicy;
+
         public CharityCommissionController(ILogger<CharityCommissionController> logger, CharityCommissionApiClient apiClient)
         {
             _logger = logger;
             _apiClient = apiClient;
+            _retryPolicy = GetRetryPolicy();
         }
 
         [Route("charity-commission-lookup")]
         public async Task<IActionResult> CharityDetails(int charityNumber)
         {
-            var charityDetails = await _apiClient.GetCharity(charityNumber);
+            var charityDetails = await _retryPolicy.ExecuteAsync(context => _apiClient.GetCharity(charityNumber), new Context());
 
             if (charityDetails == null)
             {
@@ -28,6 +34,21 @@
             }
 
             return Ok(charityDetails);
+        }
+
+        private AsyncRetryPolicy GetRetryPolicy()
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(4)
+                }, (exception, timeSpan, retryCount, context) =>
+                {
+                    _logger.LogWarning($"Error retrieving response from Charity Commission API. Reason: {exception.Message}. Retrying in {timeSpan.Seconds} secs...attempt: {retryCount}");
+                });
         }
     }
 }
--- src/SFA.DAS.ApplyService.InternalApi/Controllers/CompaniesHouseController.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Controllers/CompaniesHouseController.cs
@@ -4,6 +4,9 @@
     using Microsoft.Extensions.Logging;
     using SFA.DAS.ApplyService.InternalApi.Infrastructure;
     using System.Threading.Tasks;
+    using Polly;
+    using Polly.Retry;
+    using System;
 
     public class CompaniesHouseController : Controller
     {
@@ -11,16 +14,19 @@
 
         private CompaniesHouseApiClient _apiClient;
 
+        private AsyncRetryPolicy _retryPolicy;
+
         public CompaniesHouseController(ILogger<CompaniesHouseController> logger, CompaniesHouseApiClient apiClient)
         {
             _logger = logger;
             _apiClient = apiClient;
+            _retryPolicy = GetRetryPolicy();
         }
 
         [Route("companies-house-lookup")]
         public async Task<IActionResult> CompanyDetails(string companyNumber)
         {
-            var companyDetails = await _apiClient.GetCompany(companyNumber);
+            var companyDetails = await _retryPolicy.ExecuteAsync(context => _apiClient.GetCompany(companyNumber), new Context());
 
             if (companyDetails == null)
             {
@@ -28,6 +34,23 @@
             }
 
             return Ok(companyDetails);
+        }
+
+        private AsyncRetryPolicy GetRetryPolicy()
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(4)
+                },
+                (exception, timeSpan, retryCount, context) =>
+                {
+                    _logger.LogWarning(
+                        $"Error retrieving response from Companies House API. Reason: {exception.Message}. Retrying in {timeSpan.Seconds} secs...attempt: {retryCount}");
+                });
         }
     }
 }
