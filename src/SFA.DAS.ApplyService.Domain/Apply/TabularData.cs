4431a6f931e8496cb0026b634ee68af5066adc00
Dave Read <david.read@digital.education.gov.uk>
2019-08-01 15:55:39 +0100

    APR-613 create tabular data control for apply journey

---
+++ src/SFA.DAS.ApplyService.Domain/Apply/TabularData.cs
@@ -0,0 +1,17 @@
+﻿
+namespace SFA.DAS.ApplyService.Domain.Apply
+{
+    using System.Collections.Generic;
+    
+    public class TabularData
+    {
+        public string Caption { get; set; }
+        public List<string> HeadingTitles { get; set; }
+        public List<TabularDataRow> DataRows { get; set; }
+    }
+
+    public class TabularDataRow
+    {
+        public List<string> Columns { get; set; }
+    }
+}
