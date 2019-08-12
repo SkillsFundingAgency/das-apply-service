f1e340a649871b0a340bba4657ceea876cd3ccb4
Dave Read <david.read@digital.education.gov.uk>
2019-08-12 15:46:30 +0100

    APR-512 add styling for section progress - interim commit

--- src/SFA.DAS.ApplyService.Web/ViewModels/Roatp/TaskListViewModel.cs
+++ src/SFA.DAS.ApplyService.Web/ViewModels/Roatp/TaskListViewModel.cs
@@ -3,6 +3,7 @@ namespace SFA.DAS.ApplyService.Web.ViewModels.Roatp
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     public class TaskListViewModel
     {
@@ -10,5 +11,59 @@ namespace SFA.DAS.ApplyService.Web.ViewModels.Roatp
         public string UKPRN { get; set; }
         public string OrganisationName { get; set; }
         public IEnumerable<Domain.Entities.ApplicationSequence> ApplicationSequences { get; set; }
+
+        public string CssClass(int sequenceId, int sectionId)
+        {
+            var status = SectionStatus(sequenceId, sectionId);
+
+            if (status == String.Empty)
+            {
+                return "hidden";
+            }
+
+            var cssClass = status.ToLower();
+            cssClass = cssClass.Replace(" ", "");
+            
+            return cssClass;
+        }
+
+        public string SectionStatus(int sequenceId, int sectionId)
+        {
+            var sequence = ApplicationSequences.FirstOrDefault(x => (int)x.SequenceId == sequenceId);
+            if (sequence == null)
+            {
+                return String.Empty;
+            }
+
+            var section = sequence.Sections.FirstOrDefault(x => x.SectionId == sectionId);
+            if (section == null)
+            {
+                return string.Empty;
+            }
+
+            var pages = section.QnAData.Pages;
+            foreach (var page in pages)
+            {
+                var questionIds = page.Questions.Select(x => x.QuestionId);
+                foreach (var questionId in questionIds)
+                {
+                    if (!page.PageOfAnswers.Any())
+                    {
+                        return "In Progress";
+                    }
+
+                    foreach (var pageOfAnswers in page.PageOfAnswers)
+                    {
+                        var matchedAnswer = pageOfAnswers.Answers.FirstOrDefault(y => y.QuestionId == questionId);
+                        if (matchedAnswer == null || String.IsNullOrEmpty(matchedAnswer.Value))
+                        {
+                            return "In Progress";
+                        }
+                    }
+                }
+            }
+
+            return "Completed";
+        }
     }
 }
