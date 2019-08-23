173063e29ebc9ecfddf5675489212810887da1db
Dave Read <david.read@digital.education.gov.uk>
2019-08-23 15:28:18 +0100

    APR-501 updated validation message for website URL format

--- src/RoatpWorkflow.sql
+++ src/RoatpWorkflow.sql
@@ -1103,7 +1103,7 @@ VALUES
 						{
 							"Name": "Regex",
 							"Value": "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$",
-							"ErrorMessage" : "Enter a website using only letters numbers and - . /"
+							"ErrorMessage" : "Enter a website using the correct format. For example, www.company.co.uk"
 						}
                       ]
                     },
