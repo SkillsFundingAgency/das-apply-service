722534d8b8ba2a26976bdbf8af670e845f569816
Dave Read <david.read@digital.education.gov.uk>
2019-09-25 11:33:14 +0100

    APR-609 fixed typo in viewmodel property

--- src/SFA.DAS.ApplyService.Web/Controllers/Roatp/RoatpApplicationController.cs
+++ src/SFA.DAS.ApplyService.Web/Controllers/Roatp/RoatpApplicationController.cs
@@ -329,7 +329,7 @@ namespace SFA.DAS.ApplyService.Web.Controllers
                 UKPRN = organisationDetails.OrganisationUkprn?.ToString(),
                 OrganisationName = organisationDetails.Name,
                 VerifiedCompaniesHouse = (verifiedCompaniesHouse.Value == "TRUE"),
-                VerifiedCharityCommision = (verifiedCharityCommission.Value == "TRUE"),
+                VerifiedCharityCommission = (verifiedCharityCommission.Value == "TRUE"),
                 CompaniesHouseManualEntry = (companiesHouseManualEntry.Value == "TRUE"),
                 CharityCommissionManualEntry = (charityCommissionManualEntry.Value == "TRUE"),
                 ApplicationRouteId = providerRoute.Value
