5ebd48519049c708ad71b6f632452e2c0a9cccc7
MarkFCain <mark.cain@digital.education.gov.uk>
2019-11-06 15:39:50 +0000

    APR-1008 partial fix of issue pending QnA data changes

--- src/SFA.DAS.ApplyService.Web/Services/RoatpTaskListWorkflowService.cs
+++ src/SFA.DAS.ApplyService.Web/Services/RoatpTaskListWorkflowService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using MoreLinq;
 using NPOI.POIFS.Storage;
@@ -36,7 +37,28 @@ namespace SFA.DAS.ApplyService.Web.Services
 
             var questionsCompleted = SectionHasCompletedQuestions(section);
 
-            return SectionText(questionsCompleted, section.SectionCompleted, sequential);
+
+            // NOTES: MFC The SectionText() method can be removed once the QnA json and process conforms to writing details
+            // into the QnA, and has been fixed.  Current areas affected is all of sequence 1, and sequence 4, section 1
+            // In addition, there is probably a case for removing all calls and reads to the ApplyRepository 'Completed' calls
+            // (MarkSectionAsCompleted, IsSectionCompleted,RemoveSectionCompleted), and the table 'ApplicationWorkflow' may be dropped
+            // I will need to double check there are no other uses for this endpoint before doing that
+            var sectionTextNewWay = GetSectionText(section.PagesComplete, section.PagesActive, sequential);
+            var sectionTextOldWay = SectionText(questionsCompleted, section.SectionCompleted, sequential);
+
+
+            var sectionText = sectionTextNewWay;
+
+            if (sectionTextOldWay != sectionTextNewWay)
+            {
+                if (sequenceId == 1 || (sequenceId == 4 && sectionId <= 2))
+                {
+                    sectionText = sectionTextOldWay;
+                }
+            }
+
+            
+            return sectionText;
         }
 
         public  bool PreviousSectionCompleted(ApplicationSequence sequence, int sectionId, bool sequential)
@@ -88,6 +110,25 @@ namespace SFA.DAS.ApplyService.Web.Services
             }
 
             return answeredQuestions;
+        }
+
+        private string GetSectionText(int completed, int active, bool sequential)
+        {
+            if (completed == active)
+                return "Completed";
+
+            if (sequential && completed == 0)
+            {
+                return "Next";
+            }
+
+            if (completed > 0)
+            {
+                return "In Progress";
+            }
+
+            return string.Empty;
+
         }
 
         private  string SectionText(int completedCount, bool sectionCompleted, bool sequential)
