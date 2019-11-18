b2c03dfd1974266a222e8e62899d69a6527a06ce
Dave Read <david.read@digital.education.gov.uk>
2019-11-18 18:21:09 +0000

    APR-905 fix error message rendering for preamble pages

--- src/SFA.DAS.ApplyService.Web/ViewModels/Roatp/SearchByUkprnViewModel.cs
+++ src/SFA.DAS.ApplyService.Web/ViewModels/Roatp/SearchByUkprnViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace SFA.DAS.ApplyService.Web.ViewModels.Roatp
+﻿using SFA.DAS.ApplyService.Domain.Apply;
+using System.Collections.Generic;
+
+namespace SFA.DAS.ApplyService.Web.ViewModels.Roatp
 {
     public class SearchByUkprnViewModel
     {
         public string UKPRN { get; set; }
+        public List<ValidationErrorDetail> ErrorMessages { get; set; }
     }
 }
