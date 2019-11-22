2b5b02517c246a8214b61defa5b293827778c44a
Dave Read <david.read@digital.education.gov.uk>
2019-11-22 13:07:51 +0000

    APR-941 create email templates for confirm submit application

--- src/SFA.DAS.ApplyService.Application/Email/Consts/EmailTemplateName.cs
+++ src/SFA.DAS.ApplyService.Application/Email/Consts/EmailTemplateName.cs
@@ -31,5 +31,16 @@
         /// Requires tokens: { ApplicantFullName, UKPRN, OrganisationName, ApplicationSequence, ApplicationSection, PageTitle, GetHelpQuery }
         /// </summary>
         public const string ROATP_GET_HELP_WITH_QUESTION = "RoATPGetHelpWithQuestion";
+
+        /// <summary>
+        /// Requires tokens: { ApplicantFullName }
+        /// </summary>
+        public const string ROATP_APPLICATION_SUBMITTED = "RoATPApplicationSubmitted";
+
+        /// <summary>
+        /// Requires tokens: { ApplicantFullName }
+        /// </summary>
+        public const string ROATP_APPLICATION_SUBMITTED_MAIN = "RoATPApplicationSubmittedMain";
+
     }
 }
