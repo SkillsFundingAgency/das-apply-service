b714c0ea9c37b1e31f792452ce37f823eaf2b3cf
MarkFCain <mark.cain@digital.education.gov.uk>
2019-12-18 11:07:12 +0000

    APR-1174 adding logging

--- src/SFA.DAS.ApplyService.Web/Controllers/Roatp/RoatpApplicationController.cs
+++ src/SFA.DAS.ApplyService.Web/Controllers/Roatp/RoatpApplicationController.cs
@@ -140,8 +140,10 @@ namespace SFA.DAS.ApplyService.Web.Controllers
 
             var applicationStartResponse =
                 await _qnaApiClient.StartApplication(userId.ToString(), applicationType, startApplicationJson);
-            
+
+            _logger.LogInformation($"RoatpApplicationController.StartApplication:: Checking applicationStartResponse POST: applicationId: [{applicationStartResponse?.ApplicationId}]");
             var response = await _apiClient.StartApplication(applicationStartResponse.ApplicationId, userId, applicationType);
+            _logger.LogInformation($"RoatpApplicationController.StartApplication:: Checking response from StartApplication POST: applicationId: [{response?.ApplicationId}]");
 
             if (response != null)
             {
