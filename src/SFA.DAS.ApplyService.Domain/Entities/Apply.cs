6586b7320a45370f2d34fb4ccb3d7f62837d6f2b
Dave Read <david.read@digital.education.gov.uk>
2020-02-05 13:27:17 +0000

    APR-1269 add assessor review status

--- src/SFA.DAS.ApplyService.Domain/Entities/Apply.cs
+++ src/SFA.DAS.ApplyService.Domain/Entities/Apply.cs
@@ -105,4 +105,13 @@ namespace SFA.DAS.ApplyService.Domain.Entities
         public const string Approved = "Approved";
         public const string Declined = "Declined";
     }
+
+    public static class AssessorReviewStatus
+    {
+        public const string Draft = "Draft";
+        public const string New = "New";
+        public const string InProgress = "In Progress";
+        public const string Approved = "Approved";
+        public const string Declined = "Declined";
+    }
 }
