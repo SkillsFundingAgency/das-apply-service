26d218c7b7789ff5403cc342d8dc22a0233876c6
Alan Burns <adburns57@gmail.com>
2020-02-05 15:21:26 +0000

    ooops over-eager cleardown on Email Templates

--- src/SFA.DAS.ApplyService.Database/Script.PostDeployment1.sql
+++ src/SFA.DAS.ApplyService.Database/Script.PostDeployment1.sql
@@ -13,6 +13,12 @@ Post-Deployment Script Template
 -- cleanup (for now)
 DELETE FROM [EmailTemplates]
 
+IF NOT EXISTS (SELECT * FROM EmailTemplates WHERE TemplateName = N'RoATPGetHelpWithQuestion')
+BEGIN
+	INSERT INTO EmailTemplates ([Id], [Status],[TemplateName],[TemplateId],[Recipients],[CreatedAt],[CreatedBy]) 
+	VALUES (NEWID(), 'Live', N'RoATPGetHelpWithQuestion', N'9d1e1a7e-3557-4781-8901-ea627ae70ec2', N'RoATP.SUPPORT@education.gov.uk', GETDATE(), 'System')
+END
+
 IF NOT EXISTS (SELECT * FROM EmailTemplates WHERE TemplateName = N'RoATPApplicationSubmitted')
 BEGIN
 	INSERT INTO EmailTemplates ([Id], [Status],[TemplateName],[TemplateId],[CreatedAt],[CreatedBy]) 
