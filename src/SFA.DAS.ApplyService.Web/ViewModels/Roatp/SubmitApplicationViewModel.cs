0d630cbeba1c29461dd74de3d3e8598dc8a3c2f5
Anatoli Malev <makcom@hotmail.co.uk>
2020-02-10 09:24:20 +0000

    Changing text as content was updated

--- src/SFA.DAS.ApplyService.Web/ViewModels/Roatp/SubmitApplicationViewModel.cs
+++ src/SFA.DAS.ApplyService.Web/ViewModels/Roatp/SubmitApplicationViewModel.cs
@@ -9,7 +9,7 @@ namespace SFA.DAS.ApplyService.Web.ViewModels.Roatp
     {
         public Guid ApplicationId { get; set; }
         public string OrganisationName { get; set; }
-        [Range(typeof(bool), "true", "true", ErrorMessage = "Tell us if you confirm that all the answers and uploads in this application are accurate and to the best of your knowledge")]
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Tell us if you confirm that all the answers and file uploads in this application are true and accurate to the best of your knowledge")]
         public bool ConfirmSubmitApplication { get; set; }
         [Range(typeof(bool), "true", "true", ErrorMessage = "Tell us if you agree to give further information on any application answers within 5 working days when requested by ESFA")]
         public bool ConfirmFurtherInfoSubmitApplication { get; set; }
