42a5beb95484c88d32348257d48ceb73cdf1b512
Martin Lush <martin-hornsby@ntlworld.com>
2020-02-19 11:41:01 +0000

    Sync'ed status consts for RoATP

--- src/SFA.DAS.ApplyService.Domain/Entities/Apply.cs
+++ src/SFA.DAS.ApplyService.Domain/Entities/Apply.cs
@@ -76,9 +76,11 @@ namespace SFA.DAS.ApplyService.Domain.Entities
     }
   
     public class ApplicationStatus	
-    {	
+    {
+        public const string New = "New";
         public const string InProgress = "In Progress";	
-        public const string Submitted = "Submitted";	
+        public const string Submitted = "Submitted";
+        public const string Resubmitted = "Resubmitted";
         public const string GatewayAssessed = "GatewayAssessed";	
         public const string FeedbackAdded = "FeedbackAdded";	
         public const string Rejected = "Rejected";	
