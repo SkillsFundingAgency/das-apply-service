506a16a3d7005c9d35a76477a69972bcbb509e21
MarkFCain <mark.cain@digital.education.gov.uk>
2020-02-20 09:35:40 +0000

    temp fix to token service

--- src/SFA.DAS.ApplyService.InternalApi/Infrastructure/RoatpTokenService.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Infrastructure/RoatpTokenService.cs
@@ -1,4 +1,6 @@
-﻿namespace SFA.DAS.ApplyService.InternalApi.Infrastructure
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace SFA.DAS.ApplyService.InternalApi.Infrastructure
 {
     using Configuration;
     using Microsoft.IdentityModel.Clients.ActiveDirectory;
@@ -6,14 +8,18 @@
     public class RoatpTokenService : IRoatpTokenService
     {
         private readonly IApplyConfig _configuration;
+        private readonly IHostingEnvironment _hostingEnvironment;
 
-        public RoatpTokenService(IConfigurationService configurationService)
+        public RoatpTokenService(IConfigurationService configurationService, IHostingEnvironment hostingEnvironment)
         {
+            _hostingEnvironment = hostingEnvironment;
             _configuration = configurationService.GetConfig().Result;
         }
 
         public string GetToken()
         {
+            if (_hostingEnvironment.IsDevelopment())
+                return string.Empty;
             var tenantId = _configuration.RoatpApiAuthentication.TenantId;
             var clientId = _configuration.RoatpApiAuthentication.ClientId;
             var appKey = _configuration.RoatpApiAuthentication.ClientSecret;
