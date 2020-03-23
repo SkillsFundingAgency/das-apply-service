83406aa59403b4ee92e1575a2ec968d44ef548c3
Dave Read <david.read@digital.education.gov.uk>
2020-03-23 10:41:40 +0000

    APR-1444 changed to use text page id instead of n-nn

--- src/SFA.DAS.ApplyService.Domain/Roatp/GatewayPageIds.cs
+++ src/SFA.DAS.ApplyService.Domain/Roatp/GatewayPageIds.cs
@@ -6,19 +6,18 @@ namespace SFA.DAS.ApplyService.Domain.Roatp
 {
     public static class GatewayPageIds
     {
-        public const string LegalName = "1-10";
-        public const string TradingName = "1-20";
-        public const string OrganisationStatus = "1-30";
+        public const string LegalName = "LegalName";
+        public const string TradingName = "TradingName";
+        public const string OrganisationStatus = "OrganisationStatus";
 
-        public const string WebsiteAddress = "1-60";
+        public const string WebsiteAddress = "WebsiteAddress";
 
 
-        public const string OfficeForStudent = "4-10";
-        public const string InitialTeacherTraining = "4-20";
-        public const string Ofsted = "4-30";
-        public const string SubcontractorDeclaration = "4-40";
+        public const string OfficeForStudent = "OfficeForStudents";
+        public const string InitialTeacherTraining = "InitialTeacherTraining";
+        public const string Ofsted = "Ofsted";
+        public const string SubcontractorDeclaration = "SubcontractorDeclaration";
 
-        public const string CCOrganisationCompositionCreditors = "5-10";
-
+        public const string CCOrganisationCompositionCreditors = "CompositionWithCreditors";
     }
 }
