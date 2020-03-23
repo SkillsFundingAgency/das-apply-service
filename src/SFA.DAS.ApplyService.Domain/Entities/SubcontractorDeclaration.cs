767102eaf93ee193a42e6d1bbf0f752f2cbb40d5
Michael Young <mick.young1981@googlemail.com>
2020-03-23 14:06:45 +0000

    Added subcontractor declaration model.

---
+++ src/SFA.DAS.ApplyService.Domain/Entities/SubcontractorDeclaration.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.ApplyService.Domain.Entities
+{
+    public class SubcontractorDeclaration
+    {
+        public bool HasDeliveredTrainingAsSubcontractor { get; set; }
+
+        public string ContractFileName { get; set; }
+
+        public byte[] ContractFile { get; set; }
+    }
+}
