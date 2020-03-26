b0d751d34697d46190fea416fc0a5cd7f9ded8a8
Martin Lush <martin-hornsby@ntlworld.com>
2020-03-26 11:23:47 +0000

    APR-1477 - Added unit tests for Open, Clarification and Closed FHA Handlers

---
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/Financial/ClosedFinancialApplicationsHandlerTests/ClosedFinancialApplicationsHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApplyService.Application.Apply;
+using SFA.DAS.ApplyService.Application.Apply.Financial.Applications;
+using SFA.DAS.ApplyService.Domain.Apply;
+using SFA.DAS.ApplyService.Domain.Entities;
+
+namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.Financial.ClosedFinancialApplicationsHandlerTests
+{
+    [TestFixture]
+    public class ClosedFinancialApplicationsHandlerTests
+    {
+        private Mock<IApplyRepository> _applyRepository;
+        private ClosedFinancialApplicationsHandler _handler;
+
+        private List<RoatpFinancialSummaryItem> _closedApplications;
+
+        [SetUp]
+        public void Setup()
+        {
+            var closedFinancialApplication = new RoatpFinancialSummaryItem { ApplicationStatus = ApplicationStatus.GatewayAssessed, FinancialReviewStatus = FinancialReviewStatus.Approved, SubmittedDate = DateTime.Now, ClosedDate = DateTime.Now };
+
+            _closedApplications = new List<RoatpFinancialSummaryItem> { closedFinancialApplication };
+
+            _applyRepository = new Mock<IApplyRepository>();
+            _applyRepository.Setup(r => r.GetClosedFinancialApplications()).ReturnsAsync(_closedApplications);
+
+            _handler = new ClosedFinancialApplicationsHandler(_applyRepository.Object);
+        }
+
+        [Test]
+        public async Task OClosedFinancialApplicationsHandler_returns_expected_applications()
+        {
+            var request = new ClosedFinancialApplicationsRequest();
+            var result = await _handler.Handle(request, new CancellationToken());
+
+            result.Should().NotBeNull();
+            result.Should().BeEquivalentTo(_closedApplications);
+        }
+    }
+}
