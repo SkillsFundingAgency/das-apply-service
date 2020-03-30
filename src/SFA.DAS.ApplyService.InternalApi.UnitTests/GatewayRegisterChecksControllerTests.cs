cb3ad0aa70b3be70c634d8e7a430074a1260e892
Martin Lush <martin-hornsby@ntlworld.com>
2020-03-30 16:21:13 +0100

    APR-1294 - Added endpoints and unit test for Gateway Register Checks

---
+++ src/SFA.DAS.ApplyService.InternalApi.UnitTests/GatewayRegisterChecksControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApplyService.Application.Apply.GetApplications;
+using SFA.DAS.ApplyService.Application.Apply.Roatp;
+using SFA.DAS.ApplyService.InternalApi.Controllers;
+using SFA.DAS.ApplyService.InternalApi.Infrastructure;
+
+namespace SFA.DAS.ApplyService.InternalApi.UnitTests
+{
+    [TestFixture]
+    public class GatewayRegisterChecksControllerTests
+    {
+        private readonly Guid _applicationId = Guid.NewGuid();
+
+        private Mock<IInternalQnaApiClient> _qnaApiClient;
+        private Mock<IMediator> _mediator;
+        private Mock<ILogger<GatewayRegisterChecksController>> _logger;
+        private GatewayRegisterChecksController _controller;
+
+        [SetUp]
+        public void Before_each_test()
+        {
+            _qnaApiClient = new Mock<IInternalQnaApiClient>();
+            _mediator = new Mock<IMediator>();
+            _logger = new Mock<ILogger<GatewayRegisterChecksController>>();
+            _controller = new GatewayRegisterChecksController(_qnaApiClient.Object, _mediator.Object, _logger.Object);
+        }
+
+        [TestCase("1")]
+        [TestCase("2")]
+        [TestCase("3")]
+        public async Task GetProviderRoute_returns_expected_value(string providerRoute)
+        {
+            _qnaApiClient
+               .Setup(x => x.GetAnswerValue(_applicationId,
+                                           RoatpWorkflowSequenceIds.Preamble,
+                                           RoatpWorkflowSectionIds.Preamble,
+                                           RoatpWorkflowPageIds.ProviderRoute,
+                                           RoatpPreambleQuestionIdConstants.ApplyProviderRoute)).ReturnsAsync(providerRoute);
+
+            var actualResult = await _controller.GetProviderRoute(_applicationId);
+            Assert.AreEqual(providerRoute, actualResult);
+        }
+
+        [TestCase("Main")]
+        [TestCase("Supporting")]
+        [TestCase("Employer")]
+        public async Task GetProviderRouteName_returns_expected_value(string providerRouteName)
+        {
+            var application = new Domain.Entities.Apply
+            {
+                ApplicationId = _applicationId,
+                ApplyData = new Domain.Entities.ApplyData
+                {
+                    ApplyDetails = new Domain.Entities.ApplyDetails
+                    {
+                        ProviderRouteName = providerRouteName
+                    }
+                }
+            };
+
+            _mediator
+               .Setup(x => x.Send(It.Is<GetApplicationRequest>(y => y.ApplicationId == _applicationId), It.IsAny<CancellationToken>()))
+               .ReturnsAsync(application);
+
+            var actualResult = await _controller.GetProviderRouteName(_applicationId);
+
+            Assert.AreEqual(providerRouteName, actualResult);
+        }
+    }
+}
---
+++ src/SFA.DAS.ApplyService.InternalApi/Controllers/GatewayRegisterChecksController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ApplyService.Application.Apply.GetApplications;
+using SFA.DAS.ApplyService.Application.Apply.Roatp;
+using SFA.DAS.ApplyService.InternalApi.Infrastructure;
+
+namespace SFA.DAS.ApplyService.InternalApi.Controllers
+{
+    [Authorize]
+    public class GatewayRegisterChecksController : Controller
+    {
+        private readonly IInternalQnaApiClient _qnaApiClient;
+        private readonly IMediator _mediator;
+        private readonly ILogger<GatewayRegisterChecksController> _logger;
+
+        public GatewayRegisterChecksController(IInternalQnaApiClient qnaApiClient, IMediator mediator, ILogger<GatewayRegisterChecksController> logger)
+        {
+            _qnaApiClient = qnaApiClient;
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet("/Gateway/{applicationId}/ProviderRoute")]
+        public async Task<string> GetProviderRoute(Guid applicationId)
+        {
+            var providerRoute = await _qnaApiClient.GetAnswerValue(applicationId,
+                RoatpWorkflowSequenceIds.Preamble,
+                RoatpWorkflowSectionIds.Preamble,
+                RoatpWorkflowPageIds.ProviderRoute,
+                RoatpPreambleQuestionIdConstants.ApplyProviderRoute);
+
+            _logger.LogInformation($"Getting ProviderRoute for application '{applicationId}' - ProviderRoute '{providerRoute}'");
+
+            return providerRoute;
+        }
+
+        [HttpGet("/Gateway/{applicationId}/ProviderRouteName")]
+        public async Task<string> GetProviderRouteName(Guid applicationId)
+        {
+            // NOTE: ProviderRouteName name isn't part of QnA. It's actually part of the Apply Details!
+            var application = await _mediator.Send(new GetApplicationRequest(applicationId));
+
+            var providerRouteName = application?.ApplyData?.ApplyDetails?.ProviderRouteName;
+
+            _logger.LogInformation($"Getting ProviderRouteName for application '{applicationId}' - ProviderRouteName '{providerRouteName}'");
+
+            return providerRouteName;
+        }
+    }
+}
