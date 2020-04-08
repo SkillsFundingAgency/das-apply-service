beb2d87151f6321a5be95744420bda8ebc2dac25
Dave Read <david.read@digital.education.gov.uk>
2020-04-08 17:52:20 +0100

    APR-1356 add unit test coverage for organisation verification service

---
+++ src/SFA.DAS.ApplyService.Web.UnitTests/RoatpOrganisationVerificationServiceTests.cs
@@ -0,0 +1,220 @@
+﻿using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApplyService.Application.Apply.Roatp;
+using SFA.DAS.ApplyService.Domain.Entities;
+using SFA.DAS.ApplyService.Web.Infrastructure;
+using SFA.DAS.ApplyService.Web.Services;
+using System;
+using System.Collections.Generic;
+
+namespace SFA.DAS.ApplyService.Web.UnitTests
+{
+    [TestFixture]
+    public class RoatpOrganisationVerificationServiceTests
+    {
+        private Mock<IQnaApiClient> _qnaApiClient;
+        private RoatpOrganisationVerificationService _service;
+        private Guid _applicationId;
+        private Guid _preambleSequenceId;
+        private Guid _yourOrganisationSequenceId;
+        private Guid _preambleSectionId;
+        private Guid _whosInControlSectionId;
+
+        [SetUp]
+        public void Before_each_test()
+        {
+            _applicationId = Guid.NewGuid();
+
+            _preambleSequenceId = Guid.NewGuid();
+            _yourOrganisationSequenceId = Guid.NewGuid();
+            _preambleSectionId = Guid.NewGuid();
+            _whosInControlSectionId = Guid.NewGuid();
+
+            _qnaApiClient = new Mock<IQnaApiClient>();
+
+            var sequences = new List<ApplicationSequence>
+            {
+                new ApplicationSequence
+                {
+                    SequenceId = RoatpWorkflowSequenceIds.Preamble,
+                    Id = _preambleSequenceId
+                },
+                new ApplicationSequence
+                {
+                    SequenceId = RoatpWorkflowSequenceIds.YourOrganisation,
+                    Id = _yourOrganisationSequenceId
+                }
+            };
+
+            _qnaApiClient.Setup(x => x.GetSequences(_applicationId)).ReturnsAsync(sequences);
+
+            var preambleSections = new List<ApplicationSection>
+            {
+                new ApplicationSection
+                {
+                    Id = _preambleSectionId,
+                    SectionId = RoatpWorkflowSectionIds.Preamble
+                }
+            };
+            _qnaApiClient.Setup(x => x.GetSections(_applicationId, _preambleSequenceId)).ReturnsAsync(preambleSections);
+
+            var yourOrganisationSections = new List<ApplicationSection>
+            {
+                new ApplicationSection
+                {
+                    Id = _whosInControlSectionId,
+                    SectionId = RoatpWorkflowSectionIds.YourOrganisation.WhosInControl
+                }
+            };
+            _qnaApiClient.Setup(x => x.GetSections(_applicationId, _yourOrganisationSequenceId)).ReturnsAsync(yourOrganisationSections);
+
+            _service = new RoatpOrganisationVerificationService(_qnaApiClient.Object);
+        }
+
+        [TestCase("TRUE", true)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+
+        public void Verified_companies_house_matches_answer_in_preamble(string answerValue, bool expectedVerificationResult)
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _preambleSectionId, RoatpWorkflowPageIds.Preamble,
+                                                 RoatpPreambleQuestionIdConstants.UkrlpVerificationCompany))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.VerifiedCompaniesHouse.Should().Be(expectedVerificationResult);
+        }
+
+        [TestCase("TRUE", true)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+
+        public void Verified_charity_commission_matches_answer_in_preamble(string answerValue, bool expectedVerificationResult)
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _preambleSectionId, RoatpWorkflowPageIds.Preamble,
+                                                 RoatpPreambleQuestionIdConstants.UkrlpVerificationCharity))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.VerifiedCharityCommission.Should().Be(expectedVerificationResult);
+        }
+
+        [TestCase("TRUE", true)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+
+        public void Companies_house_information_manual_entry_required_matches_answer_in_preamble(string answerValue, bool expectedVerificationResult)
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _preambleSectionId, RoatpWorkflowPageIds.Preamble,
+                                                 RoatpPreambleQuestionIdConstants.CompaniesHouseManualEntryRequired))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.CompaniesHouseManualEntry.Should().Be(expectedVerificationResult);
+        }
+
+        [TestCase("TRUE", true)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+
+        public void Charity_commission_information_manual_entry_required_matches_answer_in_preamble(string answerValue, bool expectedVerificationResult)
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _preambleSectionId, RoatpWorkflowPageIds.Preamble,
+                                                 RoatpPreambleQuestionIdConstants.CharityCommissionTrusteeManualEntry))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.CharityCommissionManualEntry.Should().Be(expectedVerificationResult);
+        }
+
+        [TestCase("Y", true)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+
+        public void Companies_house_data_confirmed_matches_answer_in_whos_in_control_pages(string answerValue, bool expectedVerificationResult)
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.CompaniesHouseStartPage,
+                                                 RoatpYourOrganisationQuestionIdConstants.CompaniesHouseDetailsConfirmed))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.CompaniesHouseDataConfirmed.Should().Be(expectedVerificationResult);
+        }
+
+        [TestCase("Y", true)]
+        [TestCase("", false)]
+        [TestCase(null, false)]
+
+        public void Charity_commission_data_confirmed_matches_answer_in_whos_in_control_pages(string answerValue, bool expectedVerificationResult)
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.CharityCommissionStartPage,
+                                                 RoatpYourOrganisationQuestionIdConstants.CharityCommissionDetailsConfirmed))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.CharityCommissionDataConfirmed.Should().Be(expectedVerificationResult);
+        }
+
+        [Test]
+        public void Whos_in_control_confirmed_is_true_if_manually_entered_sole_trader_details()
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.AddSoleTraderDob,
+                                                 RoatpYourOrganisationQuestionIdConstants.AddSoleTradeDob))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = "details" });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.WhosInControlConfirmed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Whos_in_control_confirmed_is_true_if_manually_entered_partner_details()
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.AddPartners,
+                                                 RoatpYourOrganisationQuestionIdConstants.AddPartners))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = "details" });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.WhosInControlConfirmed.Should().BeTrue();
+        }
+
+        [Test]
+        public void Whos_in_control_confirmed_is_true_if_manually_entered_psc_details()
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.AddPeopleInControl,
+                                                 RoatpYourOrganisationQuestionIdConstants.AddPeopleInControl))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = "details" });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.WhosInControlConfirmed.Should().BeTrue();
+        }
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void Whos_in_control_confirmed_is_false_if_no_manually_entered_details(string answerValue)
+        {
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.AddSoleTraderDob,
+                                                 RoatpYourOrganisationQuestionIdConstants.AddSoleTradeDob))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.AddPartners,
+                                                 RoatpYourOrganisationQuestionIdConstants.AddPartners))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+            _qnaApiClient.Setup(x => x.GetAnswer(_applicationId, _whosInControlSectionId, RoatpWorkflowPageIds.WhosInControl.AddPeopleInControl,
+                                                 RoatpYourOrganisationQuestionIdConstants.AddPeopleInControl))
+                         .ReturnsAsync(new Domain.Apply.Answer { Value = answerValue });
+
+            var verificationResult = _service.GetOrganisationVerificationStatus(_applicationId).GetAwaiter().GetResult();
+
+            verificationResult.WhosInControlConfirmed.Should().BeFalse();
+        }
+    }
+}
