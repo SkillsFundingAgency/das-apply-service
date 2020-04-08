bc5b69cdf6381b86de23325a45426dda16f85a38
Dave Read <david.read@digital.education.gov.uk>
2020-04-08 16:09:24 +0100

    APR-1356 moved workflow services tests all into the same file

--- src/SFA.DAS.ApplyService.Web.UnitTests/RoatpTaskListWorkflowServiceTests.cs
+++ src/SFA.DAS.ApplyService.Web.UnitTests/RoatpTaskListWorkflowServiceTests.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ApplyService.Application.Apply.Roatp;
+using SFA.DAS.ApplyService.Domain.Apply;
 using SFA.DAS.ApplyService.Domain.Entities;
 using SFA.DAS.ApplyService.Domain.Roatp;
 using SFA.DAS.ApplyService.Web.Configuration;
@@ -23,10 +24,12 @@ namespace SFA.DAS.ApplyService.Web.UnitTests
         private Mock<INotRequiredOverridesService> _notRequiredOverridesService;
         private Mock<IOptions<List<TaskListConfiguration>>> _configuration;
         private Mock<ILogger<RoatpTaskListWorkflowService>> _logger;
+        private Guid _applicationId;
 
         [SetUp]
         public void Before_each_test()
         {
+            _applicationId = Guid.NewGuid();
             _qnaApiClient = new Mock<IQnaApiClient>();
             _notRequiredOverridesService = new Mock<INotRequiredOverridesService>();
             _notRequiredOverridesService.Setup(x => x.GetNotRequiredOverrides(It.IsAny<Guid>())).Returns(new List<NotRequiredOverrideConfiguration>());
@@ -95,6 +98,166 @@ namespace SFA.DAS.ApplyService.Web.UnitTests
                                                         _configuration.Object, _logger.Object);
         }
 
+        [Test]
+        public void Get_SectionStatus_Empty_When_Null_Sequences_Used()
+        {
+            var expectedResult = string.Empty;
+            var actualResult = _service.SectionStatus(_applicationId, 0, 0, null, null);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Get_SectionStatus_Empty_When_Empty_Sequences_Used()
+        {
+            var expectedResult = string.Empty;
+            var actualResult = _service.SectionStatus(_applicationId, 0, 0, new List<ApplicationSequence>(), null);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test]
+        public void Get_SectionStatus_Empty_When_Empty_Sections_Used()
+        {
+            var expectedResult = string.Empty;
+            var actualResult = _service.SectionStatus(_applicationId, 1, 0, new List<ApplicationSequence> { new ApplicationSequence { ApplicationId = new Guid(), SequenceId = 1 } }, null);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Get_SectionStatus_Empty_When_Unmatched_Sections_Used()
+        {
+            var expectedResult = string.Empty;
+            var actualResult = _service.SectionStatus(_applicationId, 1, 3, new List<ApplicationSequence> { new ApplicationSequence { ApplicationId = new Guid(), SequenceId = 1, Sections = new List<ApplicationSection> { new ApplicationSection { SectionId = 2 } } } }, null);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test]
+        public void Get_SectionStatus_Empty_When_Matched_Sections_Used_But_No_Other_Setup()
+        {
+            var sectionId = 2;
+            var expectedResult = string.Empty;
+            var actualResult = _service.SectionStatus(_applicationId, 1, sectionId, new List<ApplicationSequence> { new ApplicationSequence { ApplicationId = new Guid(), SequenceId = 1,
+                Sections = new List<ApplicationSection> { new ApplicationSection { SectionId = sectionId,
+                                                                                    QnAData = new QnAData {Pages = new List<Page>()}} } } },
+                null);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [Test]
+        public void Get_SectionStatus_Not_Required_When_Matched_Sections_Used_And_Overrides_Set()
+        {
+            var sequenceId = 1;
+            var sectionId = 2;
+            var applicationRouteId = "3";
+            var expectedResult = "not required";
+            var notRequiredOverrides = new List<NotRequiredOverrideConfiguration>
+            {
+                new NotRequiredOverrideConfiguration
+                {
+                    Conditions = new List<NotRequiredCondition>
+                    {
+                        new NotRequiredCondition
+                        {
+                            ConditionalCheckField = "ProviderTypeId",
+                            MustEqual = applicationRouteId,
+                            Value = applicationRouteId
+                        }
+                    },
+                    SequenceId = sequenceId,
+                    SectionId = sectionId
+                }
+            };
+            _notRequiredOverridesService.Setup(x => x.GetNotRequiredOverrides(_applicationId)).Returns(notRequiredOverrides);
+
+
+            var actualResult = _service.SectionStatus(_applicationId, sequenceId, sectionId, new List<ApplicationSequence> { new ApplicationSequence { ApplicationId = new Guid(), SequenceId = sequenceId,
+                    Sections = new List<ApplicationSection> { new ApplicationSection { SectionId = sectionId,
+                        QnAData = new QnAData {Pages = new List<Page>()}} } } },
+                null);
+            Assert.AreEqual(expectedResult, actualResult.ToLower());
+        }
+
+        [Test]
+        public void Get_SectionStatus_Not_Required_When_Multiple_Conditions_Matched()
+        {
+            var sequenceId = 1;
+            var sectionId = 2;
+            var applicationRouteId = "3";
+            var orgType = "HEI";
+            var expectedResult = "not required";
+            var notRequiredOverrides = new List<NotRequiredOverrideConfiguration>
+            {
+                new NotRequiredOverrideConfiguration
+                {
+                    Conditions = new List<NotRequiredCondition>
+                    {
+                        new NotRequiredCondition
+                        {
+                            ConditionalCheckField = "ProviderTypeId",
+                            MustEqual = applicationRouteId,
+                            Value = applicationRouteId
+                        },
+                        new NotRequiredCondition
+                        {
+                            ConditionalCheckField = "OrganisationType",
+                            MustEqual = orgType,
+                            Value = orgType
+                        }
+                    },
+                    SequenceId = sequenceId,
+                    SectionId = sectionId
+                }
+            };
+            _notRequiredOverridesService.Setup(x => x.GetNotRequiredOverrides(_applicationId)).Returns(notRequiredOverrides);
+
+            var actualResult = _service.SectionStatus(_applicationId, sequenceId, sectionId, new List<ApplicationSequence> { new ApplicationSequence { ApplicationId = new Guid(), SequenceId = sequenceId,
+                    Sections = new List<ApplicationSection> { new ApplicationSection { SectionId = sectionId,
+                        QnAData = new QnAData {Pages = new List<Page>()}} } } },
+                null);
+            Assert.AreEqual(expectedResult, actualResult.ToLower());
+        }
+
+        [Test]
+        public void Get_SectionStatus_Only_One_Of_Multiple_Not_Required_Conditions_Matched()
+        {
+            var sequenceId = 1;
+            var sectionId = 2;
+            var applicationRouteId = "3";
+            var orgType = "HEI";
+
+            var notRequiredOverrides = new List<NotRequiredOverrideConfiguration>
+            {
+                new NotRequiredOverrideConfiguration
+                {
+                    Conditions = new List<NotRequiredCondition>
+                    {
+                        new NotRequiredCondition
+                        {
+                            ConditionalCheckField = "ProviderTypeId",
+                            MustEqual = applicationRouteId,
+                            Value = applicationRouteId
+                        },
+                        new NotRequiredCondition
+                        {
+                            ConditionalCheckField = "OrganisationType",
+                            MustEqual = orgType,
+                            Value = "unmatched"
+                        }
+                    },
+                    SequenceId = sequenceId,
+                    SectionId = sectionId
+                }
+            };
+            _notRequiredOverridesService.Setup(x => x.GetNotRequiredOverrides(_applicationId)).Returns(notRequiredOverrides);
+
+            var actualResult = _service.SectionStatus(_applicationId, sequenceId, sectionId, new List<ApplicationSequence> { new ApplicationSequence { ApplicationId = new Guid(), SequenceId = sequenceId,
+                    Sections = new List<ApplicationSection> { new ApplicationSection { SectionId = sectionId,
+                        QnAData = new QnAData {Pages = new List<Page>()}} } } },
+                null);
+            Assert.IsEmpty(actualResult);
+        }
 
         [Test]
         public void Section_status_is_Next_for_first_your_organisation_section_if_not_complete()
