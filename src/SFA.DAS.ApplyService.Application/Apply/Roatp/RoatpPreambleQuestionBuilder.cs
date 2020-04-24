af5c3fd7fda3fa05c00d9efe95d0c1b388094906
Dave Read <david.read@digital.education.gov.uk>
2020-04-24 09:49:32 +0100

    APR-1552 removed merge comments

--- src/SFA.DAS.ApplyService.Application/Apply/Roatp/RoatpPreambleQuestionBuilder.cs
+++ src/SFA.DAS.ApplyService.Application/Apply/Roatp/RoatpPreambleQuestionBuilder.cs
@@ -63,12 +63,9 @@ namespace SFA.DAS.ApplyService.Application.Apply.Roatp
         public static string AddPeopleInControl = "YO-130";
         public static string OfficeForStudents = "YO-235";
         public static string InitialTeacherTraining = "YO-240";
-<<<<<<< HEAD
         public static string IsPostGradTrainingOnlyApprenticeship = "YO-250";
 		public static string HasDeliveredTrainingAsSubcontractor = "YO-350";
         public static string ContractFileName = "YO-360";
-=======
->>>>>>> APR-1332_AddOfstedEndpoint
         public static string HasHadFullInspection = "YO-260";
         public static string ReceivedFullInspectionGradeForApprenticeships = "YO-270";
         public static string FullInspectionOverallEffectivenessGrade = "YO-280";
@@ -80,13 +77,6 @@ namespace SFA.DAS.ApplyService.Application.Apply.Roatp
         public static string FullInspectionApprenticeshipGradeNonOfsFunded = "YO-301";
         public static string GradeWithinLast3YearsOfsFunded = "YO-310";
         public static string GradeWithinLast3YearsNonOfsFunded = "YO-311";
-
-<<<<<<< HEAD
-=======
-        public static string IsPostGradTrainingOnlyApprenticeship = "YO-250";
-		public static string HasDeliveredTrainingAsSubcontractor = "YO-350";
-        public static string ContractFileName = "YO-360";
->>>>>>> APR-1332_AddOfstedEndpoint
     }
 
     public class RoatpCriminalComplianceChecksQuestionIdConstants
