3b042b962108cab216c8d806da0369529e6c84e5
Anatoli Malev <makcom@hotmail.co.uk>
2020-04-29 13:29:54 +0100

    Fixing a test

--- src/SFA.DAS.ApplyService.InternalApi.UnitTests/RoatpGatewayControllerTests.cs
+++ src/SFA.DAS.ApplyService.InternalApi.UnitTests/RoatpGatewayControllerTests.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using AutoMapper;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ApplyService.Application.Apply;
 using SFA.DAS.ApplyService.Application.Apply.Gateway;
+using SFA.DAS.ApplyService.Configuration;
 using SFA.DAS.ApplyService.Domain.Entities;
+using SFA.DAS.ApplyService.InternalApi.AutoMapper;
 using SFA.DAS.ApplyService.InternalApi.Controllers;
 using SFA.DAS.ApplyService.InternalApi.Infrastructure;
 using SFA.DAS.ApplyService.InternalApi.Services;
@@ -19,24 +22,44 @@ namespace SFA.DAS.ApplyService.InternalApi.UnitTests
     {
         private Mock<IApplyRepository> _applyRepository;
         private Mock<ILogger<RoatpGatewayController>> _logger;
+        private Mock<ILogger<GatewayApiChecksService>> _serviceLogger;
         private Mock<CompaniesHouseApiClient> _companiesHouseApiClient;
         private Mock<CharityCommissionApiClient> _charityCommissionApiClient;
-        private Mock<RoatpApiClient> _roatpApiClient;
+        private Mock<IRoatpApiClient> _roatpApiClient;
         private Mock<IInternalQnaApiClient> _qnaApiClient;
         private IGatewayApiChecksService _gatewayApiChecksService;
         private RoatpGatewayController _controller;
 
+        [OneTimeSetUp]
+        public void Before_all_tests()
+        {
+            Mapper.Reset();
+
+            Mapper.Initialize(cfg =>
+            {
+
+                cfg.AddProfile<UkrlpCharityCommissionProfile>();
+                cfg.AddProfile<UkrlpCompaniesHouseProfile>();
+            });
+
+            Mapper.AssertConfigurationIsValid();
+        }
+
         [SetUp]
         public void Before_each_test()
         {
+            var configurationService = new Mock<IConfigurationService>();
+            configurationService.Setup(x => x.GetConfig()).ReturnsAsync(new ApplyConfig { RoatpApiAuthentication = new RoatpApiAuthentication { ApiBaseAddress = "https://localhost" } });
+
             _applyRepository = new Mock<IApplyRepository>();
             _logger = new Mock<ILogger<RoatpGatewayController>>();
+            _serviceLogger = new Mock<ILogger<GatewayApiChecksService>>();
             _companiesHouseApiClient = new Mock<CompaniesHouseApiClient>();
             _charityCommissionApiClient = new Mock<CharityCommissionApiClient>();
-            _roatpApiClient = new Mock<RoatpApiClient>();
+            _roatpApiClient = new Mock<IRoatpApiClient>();
             _qnaApiClient = new Mock<IInternalQnaApiClient>();
             _gatewayApiChecksService = new GatewayApiChecksService(_companiesHouseApiClient.Object, _charityCommissionApiClient.Object,
-                                                                   _roatpApiClient.Object, _qnaApiClient.Object);
+                                                                   _roatpApiClient.Object, _qnaApiClient.Object, _serviceLogger.Object);
 
             _controller = new RoatpGatewayController(_applyRepository.Object, _logger.Object, _gatewayApiChecksService);
         }
