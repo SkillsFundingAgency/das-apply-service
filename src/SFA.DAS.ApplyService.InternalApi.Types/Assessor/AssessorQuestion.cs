9f0f6e9dc1e897c45953072efeee202a0e8939c2
Martin Lush <martin-hornsby@ntlworld.com>
2020-05-05 11:18:39 +0100

    APR-1656 - Added classes to show Page & QnA information

---
+++ src/SFA.DAS.ApplyService.InternalApi.Types/Assessor/AssessorQuestion.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.ApplyService.InternalApi.Types.Assessor
+{
+    public class AssessorQuestion
+    {
+        public string QuestionId { get; set; }
+        public string Label { get; set; }
+        public string QuestionBodyText { get; set; }
+        public string InputType { get; set; }
+        public string InputPrefix { get; set; }
+        public string InputSuffix { get; set; }
+    }
+}
