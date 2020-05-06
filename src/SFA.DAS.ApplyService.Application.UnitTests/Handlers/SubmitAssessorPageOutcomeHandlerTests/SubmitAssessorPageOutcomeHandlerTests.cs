753f5dceba53390162b01602f1c300f75379035f
Anatoli Malev <makcom@hotmail.co.uk>
2020-05-06 17:38:01 +0100

    unit tests

---
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/SubmitAssessorPageOutcomeHandlerTests/SubmitAssessorPageOutcomeHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApplyService.Application.Apply;
+using SFA.DAS.ApplyService.Application.Apply.Assessor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.SubmitAssessorPageOutcomeHandlerTests
+{
+    [TestFixture]
+    public class SubmitAssessorPageOutcomeHandlerTests
+    {
+        private Mock<IApplyRepository> _repository;
+        private SubmitAssessorPageOutcomeHandler _handler;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            _repository = new Mock<IApplyRepository>();
+            _handler = new SubmitAssessorPageOutcomeHandler(_repository.Object, Mock.Of<ILogger<SubmitAssessorPageOutcomeHandler>>());
+        }
+
+        [Test]
+        public async Task SubmitAssessorPageOutcome_is_stored()
+        {
+            var applicationId = Guid.NewGuid();
+            var sequenceNumber = 1;
+            var sectionNumber = 2;
+            var pageId = "30";
+            var assessorType = 1;
+            var userId = "4fs7f-userId-7gfhh";
+            var status = "Fail";
+            var comment = "Very bad";
+
+            await _handler.Handle(new SubmitAssessorPageOutcomeHandlerRequest(applicationId, sequenceNumber, sectionNumber, pageId, assessorType, userId, status, comment), new CancellationToken());
+
+            _repository.Verify(x => x.SubmitAssessorPageOutcome(applicationId, sequenceNumber, sectionNumber, pageId, assessorType, userId, status, comment), Times.Once);
+        }
+    }
+}
