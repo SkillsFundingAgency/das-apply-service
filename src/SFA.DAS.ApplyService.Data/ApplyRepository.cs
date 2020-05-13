9ce6f430eb8ac04b238372f594b16c0a6462b872
MarkFCain <mark.cain@digital.education.gov.uk>
2020-05-13 14:52:03 +0100

    APR-1569 fixing merge issues in ApplyRepository

--- src/SFA.DAS.ApplyService.Data/ApplyRepository.cs
+++ src/SFA.DAS.ApplyService.Data/ApplyRepository.cs
@@ -448,13 +448,12 @@ namespace SFA.DAS.ApplyService.Data
                             )
                         ) s
                         WHERE s.SequenceNo = @financialHealthSequence AND s.NotRequired = 'false'
-                        AND apply.ApplicationStatus = @applicationStatusGatewayAssessed AND apply.DeletedAt IS NULL
-                        AND apply.FinancialReviewStatus IN ( @financialStatusDraft, @financialStatusNew, @financialStatusInProgress)",
+                        AND apply.ApplicationStatus = @applicationStatusGatewayAssessed
+                        AND apply.FinancialReviewStatus IN (@financialStatusNew, @financialStatusInProgress)",
                         new
                         {
                             financialHealthSequence = 2,
                             applicationStatusGatewayAssessed = ApplicationStatus.GatewayAssessed,
-                            financialStatusDraft = FinancialReviewStatus.Draft,
                             financialStatusNew = FinancialReviewStatus.New,
                             financialStatusInProgress = FinancialReviewStatus.InProgress
                         })).ToList();
@@ -496,7 +495,7 @@ namespace SFA.DAS.ApplyService.Data
                                 [NotRequired] nvarchar(max) '$.NotRequired'
                             )
                         ) s
-                        WHERE s.SequenceNo = @financialHealthSequence AND s.NotRequired = 'false'
+                        WHERE s.SequenceNo = @financialHealthSequence
                         AND apply.DeletedAt IS NULL
                         AND apply.FinancialReviewStatus IN ( @financialStatusClarificationSent )",
                         new
@@ -544,14 +543,13 @@ namespace SFA.DAS.ApplyService.Data
                         ) s
                         WHERE s.SequenceNo = @financialHealthSequence AND s.NotRequired = 'false'
                         AND apply.DeletedAt IS NULL
-                        AND apply.FinancialReviewStatus IN ( @financialStatusApproved, @financialStatusDeclined, @financialStatusExempt, @financialStatusClarification )",
+                        AND apply.FinancialReviewStatus IN ( @financialStatusPass, @financialStatusFail, @financialStatusExempt )",
                        new
                        {
                            financialHealthSequence = 2,
-                           financialStatusApproved = FinancialReviewStatus.Pass,
-                           financialStatusDeclined = FinancialReviewStatus.Fail,
-                           financialStatusExempt = FinancialReviewStatus.Exempt,
-                           financialStatusClarification = FinancialReviewStatus.ClarificationSent // Place in here till we're happy with a Clarification tab in Admin Services
+                           financialStatusPass = FinancialReviewStatus.Pass,
+                           financialStatusFail = FinancialReviewStatus.Fail,
+                           financialStatusExempt = FinancialReviewStatus.Exempt
                        })).ToList();
             }
         }
