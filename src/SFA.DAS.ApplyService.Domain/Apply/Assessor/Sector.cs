85eba10bc08b4a1dd5ad6ef31662ec07f3bdf5f7
MarkFCain <mark.cain@digital.education.gov.uk>
2020-05-19 14:25:56 +0100

    APR-1660 minor change to sector.cs

--- src/SFA.DAS.ApplyService.Domain/Apply/Assessor/Sector.cs
+++ src/SFA.DAS.ApplyService.Domain/Apply/Assessor/Sector.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SFA.DAS.ApplyService.Domain.Apply.Assessor
+﻿namespace SFA.DAS.ApplyService.Domain.Apply.Assessor
 {
     public class Sector
     {
