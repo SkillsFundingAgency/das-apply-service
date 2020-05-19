b5ede049eb728dfc5fe209b7b0976b3104cb2ab6
MarkFCain <mark.cain@digital.education.gov.uk>
2020-05-19 14:28:49 +0100

    APR-1572 fixed ordering of a query

--- src/SFA.DAS.ApplyService.Data/ApplyRepository.cs
+++ src/SFA.DAS.ApplyService.Data/ApplyRepository.cs
@@ -1229,7 +1229,7 @@ namespace SFA.DAS.ApplyService.Data
 						  and AssessorReviewStatus in (@assessorReviewStatusApproved,@assessorReviewStatusDeclined)
 						  and FinancialReviewStatus in (@financialReviewStatusApproved,@financialReviewStatusDeclined, @financialReviewStatusExempt)
 						  and apply.OversightStatus IN (@oversightReviewStatusPass,@oversightReviewStatusFail)  
-                            order by JSON_VALUE(apply.ApplyData, '$.ApplyDetails.ApplicationSubmittedOn') ASC, Apply.ApplicationDeterminedDate ASC, Org.Name ASC", new
+                            order by Apply.ApplicationDeterminedDate ASC, JSON_VALUE(apply.ApplyData, '$.ApplyDetails.ApplicationSubmittedOn') ASC,  Org.Name ASC", new
                     {
                         gatewayReviewStatusApproved = SFA.DAS.ApplyService.Domain.Entities.GatewayReviewStatus.Pass,
                         assessorReviewStatusApproved = SFA.DAS.ApplyService.Domain.Entities.AssessorReviewStatus.Approved,
