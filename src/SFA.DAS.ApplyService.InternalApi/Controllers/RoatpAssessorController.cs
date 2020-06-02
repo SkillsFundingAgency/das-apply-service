86f201f79e51af41038f2d61ca71473beaa9c049
Martin Lush <martin-hornsby@ntlworld.com>
2020-06-02 14:15:01 +0100

    APR-1631 - Added API end point & unit test for UpdateAssessorReviewStatus

--- src/SFA.DAS.ApplyService.InternalApi/Controllers/RoatpAssessorController.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Controllers/RoatpAssessorController.cs
@@ -242,6 +242,12 @@ namespace SFA.DAS.ApplyService.InternalApi.Controllers
 
             return assessorReviewOutcomes;
         }
+
+        [HttpPost("Assessor/UpdateAssessorReviewStatus")]
+        public async Task UpdateAssessorReviewStatus([FromBody] UpdateAssessorReviewStatusRequest request)
+        {
+            await _mediator.Send(request);
+        }
     }
 
 
