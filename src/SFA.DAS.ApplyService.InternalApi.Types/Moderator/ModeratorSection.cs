a3cc5883c24748aebf6b1ef2f1c1e916e51a84f7
Martin Lush <martin-hornsby@ntlworld.com>
2020-08-20 14:40:43 +0100

    APR-1633 - Moderator Overview, Sectors & PageReviewOutcome

---
+++ src/SFA.DAS.ApplyService.InternalApi.Types/Moderator/ModeratorSection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFA.DAS.ApplyService.InternalApi.Types.Moderator
+{
+    public class ModeratorSection
+    {
+        public Guid Id { get; set; }
+        public int SectionNumber { get; set; }
+        public string LinkTitle { get; set; }
+        public string Status { get; set; }
+    }
+}
