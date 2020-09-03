5106a00d504ab0b307bf6a1ce3efbc00d062990a
MarkFCain <mark.cain@digital.education.gov.uk>
2020-09-03 14:40:50 +0100

    APR-1484 removed commented out code

--- src/SFA.DAS.ApplyService.Web/Controllers/Roatp/RoatpApplicationController.cs
+++ src/SFA.DAS.ApplyService.Web/Controllers/Roatp/RoatpApplicationController.cs
@@ -534,9 +534,6 @@ namespace SFA.DAS.ApplyService.Web.Controllers
         {
             bool canUpdate = false;
 
-            //var applications = await _apiClient.GetApplications(await _userService.GetSignInId(), false);
-            //var application = applications?.FirstOrDefault(app => app.ApplicationId == applicationId);
-
             var application = await _apiClient.GetApplicationByUser(applicationId, await _userService.GetSignInId());
 
             var validApplicationStatuses = new string[] { ApplicationStatus.InProgress, ApplicationStatus.FeedbackAdded };
