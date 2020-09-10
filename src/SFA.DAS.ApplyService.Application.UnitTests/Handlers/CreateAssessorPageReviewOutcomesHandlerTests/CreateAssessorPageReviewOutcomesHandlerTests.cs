1b786fb155e8122b46315aacc42a2e276215518f
chrisfoster186 <chrisfoster186@googlemail.com>
2020-09-10 15:32:17 +0100

    Adds test for handler

---
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/CreateAssessorPageReviewOutcomesHandlerTests/CreateAssessorPageReviewOutcomesHandlerTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApplyService.Application.Apply;
+using SFA.DAS.ApplyService.Application.Apply.Assessor;
+using SFA.DAS.ApplyService.Domain.Apply.Assessor;
+
+namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.CreateAssessorPageReviewOutcomesHandlerTests
+{
+    [TestFixture]
+    public class CreateAssessorPageReviewOutcomesHandlerTests
+    {
+        private Mock<IAssessorRepository> _repository;
+        private CreateAssessorPageReviewOutcomesHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _repository = new Mock<IAssessorRepository>();
+            _handler = new CreateAssessorPageReviewOutcomesHandler(_repository.Object, Mock.Of<ILogger<CreateAssessorPageReviewOutcomesHandler>>());
+        }
+
+
+        [Test]
+        public async Task CreateAssessorPageOutcomes_are_stored()
+        {
+            var request = new CreateAssessorPageReviewOutcomesRequest
+            {
+                AssessorPageReviewOutcomes = new List<AssessorPageReviewOutcome>
+                {
+                    new AssessorPageReviewOutcome
+                    {
+                        ApplicationId = Guid.NewGuid(),
+                        SequenceNumber = 1,
+                        SectionNumber = 2,
+                        AssessorNumber = 1,
+                        Comment = string.Empty,
+                        PageId = Guid.NewGuid().ToString(),
+                        Status = string.Empty,
+                        UserId = Guid.NewGuid().ToString()
+                    },
+                    new AssessorPageReviewOutcome
+                    {
+                        ApplicationId = Guid.NewGuid(),
+                        SequenceNumber = 3,
+                        SectionNumber = 4,
+                        AssessorNumber = 2,
+                        Comment = string.Empty,
+                        PageId = Guid.NewGuid().ToString(),
+                        Status = string.Empty,
+                        UserId = Guid.NewGuid().ToString()
+                    }
+                }
+            };
+
+            await _handler.Handle(request, new CancellationToken());
+
+            foreach (var outcome in request.AssessorPageReviewOutcomes)
+            {
+                _repository.Verify(x => x.SubmitAssessorPageOutcome(outcome.ApplicationId, outcome.SequenceNumber, outcome.SectionNumber, outcome.PageId, outcome.UserId, outcome.Status, outcome.Comment), Times.Once);
+            }
+            
+        }
+    }
+}
