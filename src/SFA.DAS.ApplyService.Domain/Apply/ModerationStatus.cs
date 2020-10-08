82bb56a1274b86947d4a8e2ecf54ffeca3d18590
Martin Lush <martin-hornsby@ntlworld.com>
2020-10-08 13:42:28 +0100

    APR-1872 - Added Clarification In Progress status

--- src/SFA.DAS.ApplyService.Domain/Apply/ModerationStatus.cs
+++ src/SFA.DAS.ApplyService.Domain/Apply/ModerationStatus.cs
@@ -5,6 +5,7 @@
         public const string New = "New";
         public const string InProgress = "In Progress";
         public const string ClarificationSent = "Clarification Sent";
+        public const string ClarificationInProgress = "Clarification In Progress";
         public const string Fail = "Fail";
         public const string Pass = "Pass";
     }
