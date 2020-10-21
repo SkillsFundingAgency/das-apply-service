4aed605c9ec61529548b05282d9214ffc3123632
chrisfoster186 <chrisfoster186@googlemail.com>
2020-10-21 10:48:48 +0100

    Wip -copying exising code into new service

--- src/SFA.DAS.ApplyService.Web/Services/TaskListService.cs
+++ src/SFA.DAS.ApplyService.Web/Services/TaskListService.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using SFA.DAS.ApplyService.Application.Apply.Roatp;
+using SFA.DAS.ApplyService.Domain.Entities;
+using SFA.DAS.ApplyService.Domain.Roatp;
 using SFA.DAS.ApplyService.Web.Infrastructure;
 using SFA.DAS.ApplyService.Web.ViewModels.Roatp;
 
@@ -10,11 +14,18 @@ namespace SFA.DAS.ApplyService.Web.Services
     {
         private readonly IApplicationApiClient _apiClient;
         private readonly IQnaApiClient _qnaApiClient;
+        private readonly IRoatpOrganisationVerificationService _organisationVerificationService;
+        private readonly IRoatpTaskListWorkflowService _roatpTaskListWorkflowService; //todo: consider removing this dependency, and copying the methods we need
 
-        public TaskListService(IApplicationApiClient apiClient, IQnaApiClient qnaApiClient)
+        public TaskListService(IApplicationApiClient apiClient,
+            IQnaApiClient qnaApiClient,
+            IRoatpOrganisationVerificationService organisationVerificationService,
+            IRoatpTaskListWorkflowService roatpTaskListWorkflowService)
         {
             _apiClient = apiClient;
             _qnaApiClient = qnaApiClient;
+            _organisationVerificationService = organisationVerificationService;
+            _roatpTaskListWorkflowService = roatpTaskListWorkflowService;
         }
 
         public async Task<TaskList2ViewModel> GetTaskList2ViewModel(Guid applicationId, Guid userId)
@@ -32,9 +43,60 @@ namespace SFA.DAS.ApplyService.Web.Services
                 ApplicationRouteId = providerRoute.Value,
             };
 
+            var organisationVerificationStatus = await _organisationVerificationService.GetOrganisationVerificationStatus(applicationId);
+
+            _roatpTaskListWorkflowService.RefreshNotRequiredOverrides(applicationId);
+            var sequences = _roatpTaskListWorkflowService.GetApplicationSequences(applicationId);
+
+            var yourOrganisationSequence = sequences.FirstOrDefault(x => x.SequenceId == RoatpWorkflowSequenceIds.YourOrganisation);
+
+            var yourOrganisationSequenceCompleted = CheckYourOrganisationSequenceComplete(applicationId, sequences, organisationVerificationStatus, yourOrganisationSequence);
+            var applicationSequencesCompleted = ApplicationSequencesCompleted(applicationId, sequences, organisationVerificationStatus);
+
+
+
+
 
             return result;
         }
+
+
+
+        private bool CheckYourOrganisationSequenceComplete(Guid applicationId, IEnumerable<ApplicationSequence> sequences, OrganisationVerificationStatus organisationVerificationStatus, ApplicationSequence yourOrganisationSequence)
+        {
+            var yourOrganisationSequenceComplete = true;
+            foreach (var section in yourOrganisationSequence.Sections)
+            {
+                var sectionStatus = _roatpTaskListWorkflowService.SectionStatus(applicationId, RoatpWorkflowSequenceIds.YourOrganisation,
+                    section.SectionId, sequences.ToList(), organisationVerificationStatus);
+                if (sectionStatus != TaskListSectionStatus.Completed)
+                {
+                    yourOrganisationSequenceComplete = false;
+                    break;
+                }
+            }
+
+            return yourOrganisationSequenceComplete;
+        }
+
+        private bool ApplicationSequencesCompleted(Guid applicationId, IEnumerable<ApplicationSequence> applicationSequences, OrganisationVerificationStatus organisationVerificationStatus)
+        {
+            var nonFinishSequences = applicationSequences.Where(seq => seq.SequenceId != RoatpWorkflowSequenceIds.Finish).ToList();
+            foreach (var sequence in nonFinishSequences)
+            {
+                foreach (var section in sequence.Sections)
+                {
+                    var sectionStatus = _roatpTaskListWorkflowService.SectionStatus(applicationId, sequence.SequenceId, section.SectionId, applicationSequences, organisationVerificationStatus);
+                    if (sectionStatus != TaskListSectionStatus.NotRequired && sectionStatus != TaskListSectionStatus.Completed)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
     }
 
     public interface ITaskListService
