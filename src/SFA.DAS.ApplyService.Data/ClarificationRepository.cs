cfd4b723519d4fd0f447e7a52a79ff73c759b990
Martin Lush <martin-hornsby@ntlworld.com>
2020-10-22 10:38:32 +0100

    APR-1887 - Clarification Page Review

--- src/SFA.DAS.ApplyService.Data/ClarificationRepository.cs
+++ src/SFA.DAS.ApplyService.Data/ClarificationRepository.cs
@@ -20,6 +20,92 @@ namespace SFA.DAS.ApplyService.Data
             _config = configurationService.GetConfig().GetAwaiter().GetResult();
         }
 
+        public async Task<ModerationOutcome> GetModerationOutcome(Guid applicationId, int sequenceNumber, int sectionNumber, string pageId)
+        {
+            using (var connection = new SqlConnection(_config.SqlConnectionString))
+            {
+                var moderationOutcomeResults = await connection.QueryAsync<ModerationOutcome>(
+                                                                @"SELECT  outcome.[ApplicationId]
+			                                                            , outcome.[SequenceNumber]
+			                                                            , outcome.[SectionNumber]
+			                                                            , outcome.[PageId]
+                                                                        , CASE 
+                                                                            WHEN outcome.[ModeratorUserId] = apply.[Assessor1UserId] THEN apply.[Assessor1Name]
+                                                                            WHEN outcome.[ModeratorUserId] = apply.[Assessor2UserId] THEN apply.[Assessor2Name]
+                                                                            ELSE [ModeratorUserId]
+                                                                          END AS ModeratorName
+			                                                            , outcome.[ModeratorUserId]
+			                                                            , outcome.[ModeratorReviewStatus]
+			                                                            , outcome.[ModeratorReviewComment]
+		                                                            FROM  [dbo].[ModeratorPageReviewOutcome] outcome
+                                                                    INNER JOIN [dbo].[Apply] apply ON outcome.[ApplicationId] = apply.ApplicationId
+		                                                            WHERE outcome.[ApplicationId] = @applicationId AND
+				                                                          outcome.[SequenceNumber] = @sequenceNumber AND
+				                                                          outcome.[SectionNumber] = @sectionNumber AND
+				                                                          outcome.[PageId] = @pageId",
+                    new { applicationId, sequenceNumber, sectionNumber, pageId });
+
+                return moderationOutcomeResults.FirstOrDefault();
+            }
+        }
+
+        public async Task<List<ClarificationPageReviewOutcome>> GetClarificationPageReviewOutcomesForSection(Guid applicationId, int sequenceNumber, int sectionNumber)
+        {
+            using (var connection = new SqlConnection(_config.SqlConnectionString))
+            {
+                var pageReviewOutcomeResults = await connection.QueryAsync<ClarificationPageReviewOutcome>(
+                                                                @"SELECT [ApplicationId]
+			                                                            ,[SequenceNumber]
+			                                                            ,[SectionNumber]
+			                                                            ,[PageId]
+			                                                            ,[ModeratorUserId]
+			                                                            ,[ModeratorReviewStatus]
+			                                                            ,[ModeratorReviewComment]
+			                                                            ,[ClarificationUserId] AS UserId
+                                                                        ,CASE WHEN ([ClarificationStatus] IS NULL AND [ModeratorReviewStatus] = @passModeratorStatus) THEN @passModeratorStatus
+                                                                              ELSE [ClarificationStatus]
+                                                                         END AS [Status]
+			                                                            ,[ClarificationComment] AS Comment
+			                                                            ,[ClarificationResponse]
+		                                                            FROM [dbo].[ModeratorPageReviewOutcome]
+		                                                            WHERE [ApplicationId] = @applicationId AND
+				                                                        [SequenceNumber] = @sequenceNumber AND
+				                                                        [SectionNumber] = @sectionNumber",
+                    new { applicationId, sequenceNumber, sectionNumber, passModeratorStatus = ModerationStatus.Pass });
+
+                return pageReviewOutcomeResults.ToList();
+            }
+        }
+
+        public async Task<ClarificationPageReviewOutcome> GetClarificationPageReviewOutcome(Guid applicationId, int sequenceNumber, int sectionNumber, string pageId)
+        {
+            using (var connection = new SqlConnection(_config.SqlConnectionString))
+            {
+                var pageReviewOutcomeResults = await connection.QueryAsync<ClarificationPageReviewOutcome>(
+                                                                @"SELECT [ApplicationId]
+			                                                            ,[SequenceNumber]
+			                                                            ,[SectionNumber]
+			                                                            ,[PageId]
+			                                                            ,[ModeratorUserId]
+			                                                            ,[ModeratorReviewStatus]
+			                                                            ,[ModeratorReviewComment]
+			                                                            ,[ClarificationUserId] AS UserId
+                                                                        ,CASE WHEN ([ClarificationStatus] IS NULL AND [ModeratorReviewStatus] = @passModeratorStatus) THEN @passModeratorStatus
+                                                                              ELSE [ClarificationStatus]
+                                                                         END AS [Status]
+			                                                            ,[ClarificationComment] AS Comment
+			                                                            ,[ClarificationResponse]
+		                                                            FROM [dbo].[ModeratorPageReviewOutcome]
+		                                                            WHERE [ApplicationId] = @applicationId AND
+				                                                        [SequenceNumber] = @sequenceNumber AND
+				                                                        [SectionNumber] = @sectionNumber AND
+				                                                        [PageId] = @pageId",
+                    new { applicationId, sequenceNumber, sectionNumber, pageId, passModeratorStatus = ModerationStatus.Pass });
+
+                return pageReviewOutcomeResults.FirstOrDefault();
+            }
+        }
+
         public async Task<List<ClarificationPageReviewOutcome>> GetAllClarificationPageReviewOutcomes(Guid applicationId)
         {
             using (var connection = new SqlConnection(_config.SqlConnectionString))
@@ -43,6 +129,39 @@ namespace SFA.DAS.ApplyService.Data
                     new { applicationId, passModeratorStatus = ModerationStatus.Pass });
 
                 return pageReviewOutcomeResults.ToList();
+            }
+        }
+
+        public async Task SubmitClarificationPageOutcome(Guid applicationId, int sequenceNumber, int sectionNumber, string pageId, string userId, string status, string comment, string clarificationResponse)
+        {
+            using (var connection = new SqlConnection(_config.SqlConnectionString))
+            {
+                await connection.ExecuteAsync(
+                    @"UPDATE [ModeratorPageReviewOutcome]
+			            SET [ClarificationUserId] = @userId
+                            , [ClarificationStatus] = @status
+				            , [ClarificationComment] = @comment
+                            , [ClarificationResponse] = @clarificationResponse
+                            , [ClarificationUpdatedAt] = GETUTCDATE()
+				            , [UpdatedAt] = GETUTCDATE()
+				            , [UpdatedBy] = @userId
+			            WHERE [ApplicationId] = @applicationId AND
+					          [SequenceNumber] = @sequenceNumber AND
+					          [SectionNumber] = @sectionNumber AND
+					          [PageId] = @pageId",
+                    new { applicationId, sequenceNumber, sectionNumber, pageId, userId, status, comment, clarificationResponse });
+
+                /*
+                // Future Work - Update Moderation Status from 'Clarification Sent' to 'In Clarification'
+                await connection.ExecuteAsync(
+                    @"UPDATE [Apply]
+			            SET ModerationStatus = @clarificationInProgressStatus
+                            , UpdatedAt = GETUTCDATE()
+				            , UpdatedBy = @userId
+			            WHERE ApplicationId = @applicationId AND DeletedAt IS NULL
+                              AND ModerationStatus = @clarificationSentStatus",
+                    new { applicationId, userId, clarificationInProgressStatus = ModerationStatus.ClarificationInProgress, clarificationSentStatus = ModerationStatus.ClarificationSent });
+                */
             }
         }
     }
--- src/SFA.DAS.ApplyService.InternalApi.UnitTests/Controllers/RoatpClarificationControllerTests.cs
+++ src/SFA.DAS.ApplyService.InternalApi.UnitTests/Controllers/RoatpClarificationControllerTests.cs
@@ -2,6 +2,7 @@
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ApplyService.Application.Apply.Clarification;
+using SFA.DAS.ApplyService.Application.Apply.Roatp;
 using SFA.DAS.ApplyService.Domain.Apply.Clarification;
 using SFA.DAS.ApplyService.InternalApi.Controllers;
 using SFA.DAS.ApplyService.InternalApi.Services.Assessor;
@@ -17,10 +18,16 @@ namespace SFA.DAS.ApplyService.InternalApi.UnitTests.Controllers
     public class RoatpClarificationControllerTests
     {
         private readonly Guid _applicationId = Guid.NewGuid();
+        private const int _sequenceNumber = RoatpWorkflowSequenceIds.DeliveringApprenticeshipTraining;
+        private const int _sectionNumber = RoatpWorkflowSectionIds.DeliveringApprenticeshipTraining.ManagementHierarchy;
+        private const string _pageId = RoatpWorkflowPageIds.DeliveringApprenticeshipTraining.ManagementHierarchy;
         private const string _userId = "userid";
 
         private Mock<IMediator> _mediator;
         private Mock<IAssessorSequenceService> _sequenceService;
+        private Mock<IAssessorPageService> _pageService;
+        private Mock<IAssessorSectorService> _sectorService;
+        private Mock<IAssessorSectorDetailsService> _sectorDetailsService;
 
         private RoatpClarificationController _controller;
 
@@ -29,12 +36,15 @@ namespace SFA.DAS.ApplyService.InternalApi.UnitTests.Controllers
         {
             _mediator = new Mock<IMediator>();
             _sequenceService = new Mock<IAssessorSequenceService>();
+            _pageService = new Mock<IAssessorPageService>();
+            _sectorService = new Mock<IAssessorSectorService>();
+            _sectorDetailsService = new Mock<IAssessorSectorDetailsService>();
 
-            _controller = new RoatpClarificationController(_mediator.Object, _sequenceService.Object);
+            _controller = new RoatpClarificationController(_mediator.Object, _sequenceService.Object, _pageService.Object, _sectorService.Object, _sectorDetailsService.Object);
         }
 
         [Test]
-        public async Task GetAssessorOverview_gets_expected_sequences()
+        public async Task GetClarificationOverview_gets_expected_sequences()
         {
             var expectedResult = new List<AssessorSequence>();
             _sequenceService.Setup(x => x.GetSequences(_applicationId)).ReturnsAsync(expectedResult);
@@ -43,6 +53,63 @@ namespace SFA.DAS.ApplyService.InternalApi.UnitTests.Controllers
 
             _sequenceService.Verify(x => x.GetSequences(_applicationId), Times.Once);
             CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public async Task GetModerationOutcome_returns_expected_ModerationOutcome()
+        {
+            var expectedResult = new ModerationOutcome();
+            _mediator.Setup(x => x.Send(It.Is<GetModerationOutcomeRequest>(r => r.ApplicationId == _applicationId && r.SequenceNumber == _sequenceNumber && r.SectionNumber == _sectionNumber &&
+                   r.PageId == _pageId), It.IsAny<CancellationToken>())).ReturnsAsync(expectedResult);
+
+            var actualResult = await _controller.GetModerationOutcome(_applicationId, _sequenceNumber, _sectionNumber, _pageId);
+
+            _mediator.Verify(x => x.Send(It.Is<GetModerationOutcomeRequest>(r => r.ApplicationId == _applicationId && r.SequenceNumber == _sequenceNumber && r.SectionNumber == _sectionNumber &&
+                   r.PageId == _pageId), It.IsAny<CancellationToken>()), Times.Once);
+            Assert.AreSame(expectedResult, actualResult);
+        }
+
+        [Test]
+        public async Task SubmitPageReviewOutcome_calls_mediator()
+        {
+            var request = new RoatpClarificationController.SubmitPageReviewOutcomeCommand { SequenceNumber = _sequenceNumber, SectionNumber = _sectionNumber, PageId = _pageId, UserId = _userId, Status = "Fail", Comment = "Very bad", ClarificationResponse = "Good response" };
+
+            await _controller.SubmitPageReviewOutcome(_applicationId, request);
+
+            _mediator.Verify(x => x.Send(It.Is<SubmitClarificationPageOutcomeRequest>(r => r.ApplicationId == _applicationId && r.SequenceNumber == request.SequenceNumber && r.SectionNumber == request.SectionNumber &&
+                   r.PageId == request.PageId && r.UserId == request.UserId && r.Status == request.Status && r.Comment == request.Comment && r.ClarificationResponse == request.ClarificationResponse), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Test]
+        public async Task GetPageReviewOutcome_returns_expected_PageReviewOutcome()
+        {
+            var request = new RoatpClarificationController.GetPageReviewOutcomeRequest { SequenceNumber = _sequenceNumber, SectionNumber = _sectionNumber, PageId = _pageId, UserId = _userId };
+
+            var expectedResult = new ClarificationPageReviewOutcome();
+            _mediator.Setup(x => x.Send(It.Is<GetClarificationPageReviewOutcomeRequest>(r => r.ApplicationId == _applicationId && r.SequenceNumber == request.SequenceNumber && r.SectionNumber == request.SectionNumber &&
+                   r.PageId == request.PageId && r.UserId == request.UserId), It.IsAny<CancellationToken>())).ReturnsAsync(expectedResult);
+
+            var actualResult = await _controller.GetPageReviewOutcome(_applicationId, request);
+
+            _mediator.Verify(x => x.Send(It.Is<GetClarificationPageReviewOutcomeRequest>(r => r.ApplicationId == _applicationId && r.SequenceNumber == request.SequenceNumber && r.SectionNumber == request.SectionNumber &&
+                    r.PageId == request.PageId && r.UserId == request.UserId), It.IsAny<CancellationToken>()), Times.Once);
+            Assert.AreSame(expectedResult, actualResult);
+        }
+
+        [Test]
+        public async Task GetPageReviewOutcomesForSection_returns_expected_list_of_PageReviewOutcome()
+        {
+            var request = new RoatpClarificationController.GetPageReviewOutcomesForSectionRequest { SequenceNumber = _sequenceNumber, SectionNumber = _sectionNumber, UserId = _userId };
+
+            var expectedResult = new List<ClarificationPageReviewOutcome>();
+            _mediator.Setup(x => x.Send(It.Is<GetClarificationPageReviewOutcomesForSectionRequest>(r => r.ApplicationId == _applicationId && r.SequenceNumber == request.SequenceNumber && r.SectionNumber == request.SectionNumber &&
+                   r.UserId == request.UserId), It.IsAny<CancellationToken>())).ReturnsAsync(expectedResult);
+
+            var actualResult = await _controller.GetPageReviewOutcomesForSection(_applicationId, request);
+
+            _mediator.Verify(x => x.Send(It.Is<GetClarificationPageReviewOutcomesForSectionRequest>(r => r.ApplicationId == _applicationId && r.SequenceNumber == request.SequenceNumber && r.SectionNumber == request.SectionNumber &&
+                   r.UserId == request.UserId), It.IsAny<CancellationToken>()), Times.Once);
+            Assert.AreSame(expectedResult, actualResult);
         }
 
         [Test]
@@ -59,6 +126,44 @@ namespace SFA.DAS.ApplyService.InternalApi.UnitTests.Controllers
             _mediator.Verify(x => x.Send(It.Is<GetAllClarificationPageReviewOutcomesRequest>(r => r.ApplicationId == _applicationId &&
                         r.UserId == request.UserId), It.IsAny<CancellationToken>()), Times.Once);
             Assert.AreSame(expectedResult, actualResult);
+        }
+
+        [Test]
+        public async Task GetFirstClarificationPage_returns_expected_page()
+        {
+            var expectedResult = new AssessorPage();
+            _pageService.Setup(x => x.GetPage(_applicationId, _sequenceNumber, _sectionNumber, null)).ReturnsAsync(expectedResult);
+
+            var actualResult = await _controller.GetFirstClarificationPage(_applicationId, _sequenceNumber, _sectionNumber);
+
+            _pageService.Verify(x => x.GetPage(_applicationId, _sequenceNumber, _sectionNumber, null), Times.Once);
+            Assert.AreSame(expectedResult, actualResult);
+        }
+
+        [Test]
+        public async Task GetClarificationPage_returns_expected_page()
+        {
+            var expectedResult = new AssessorPage();
+            _pageService.Setup(x => x.GetPage(_applicationId, _sequenceNumber, _sectionNumber, _pageId)).ReturnsAsync(expectedResult);
+
+            var actualResult = await _controller.GetClarificationPage(_applicationId, _sequenceNumber, _sectionNumber, _pageId);
+
+            _pageService.Verify(x => x.GetPage(_applicationId, _sequenceNumber, _sectionNumber, _pageId), Times.Once);
+            Assert.AreSame(expectedResult, actualResult);
+        }
+
+        [Test]
+        public async Task GetSectors_returns_expected_list_of_sectors()
+        {
+            var request = new RoatpClarificationController.GetSectorsRequest { UserId = _userId };
+
+            var expectedResult = new List<AssessorSector>();
+            _sectorService.Setup(x => x.GetSectorsForModerator(_applicationId, _userId)).ReturnsAsync(expectedResult);
+
+            var actualResult = await _controller.GetSectors(_applicationId, request);
+
+            _sectorService.Verify(x => x.GetSectorsForModerator(_applicationId, _userId), Times.Once);
+            CollectionAssert.AreEqual(actualResult, expectedResult);
         }
     }
 }
