6ece9789de588f5e2d63916734b895b3629ca1fa
MarkFCain <mark.cain@digital.education.gov.uk>
2020-11-16 09:47:35 +0000

    APR-1942 snagging from TR

--- src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/ClarificationFileUploadHandlerTests/ClarificationFileUploadHandlerTests.cs
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/ClarificationFileUploadHandlerTests/ClarificationFileUploadHandlerTests.cs
@@ -2,14 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ApplyService.Application.Apply;
-using SFA.DAS.ApplyService.Application.Apply.Assessor;
 using SFA.DAS.ApplyService.Application.Apply.Financial;
-using SFA.DAS.ApplyService.Domain.Apply.Assessor;
 using SFA.DAS.ApplyService.Domain.Entities;
 
 namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.ClarificationFileUploadHandlerTests
@@ -19,14 +16,12 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.ClarificationFileU
     {
         private Mock<IApplyRepository> _repository;
         private ClarificationFileUploadHandler _handler;
-        private Guid _applicationId = Guid.NewGuid();
-        private FinancialReviewDetails _financialGrade;
+        private readonly Guid _applicationId = Guid.NewGuid();
         private ClarificationFileUploadRequest _request;
         private string _fileName = "file.pdf";
         [SetUp]
         public void TestSetup()
         {
-            
             _request = new ClarificationFileUploadRequest(_applicationId,_fileName);
             _repository = new Mock<IApplyRepository>();
             _handler = new ClarificationFileUploadHandler(_repository.Object, Mock.Of<ILogger<ClarificationFileUploadHandler>>());
@@ -34,14 +29,10 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.ClarificationFileU
 
 
         [Test]
-        public async Task Update_Financial_Grade_with_clarification_Files_name_when_no_files_present()
+        public void Update_Financial_Grade_with_clarification_Files_name_when_no_files_present()
         {
-            var financialGrade = new FinancialReviewDetails();
             var application = new Domain.Entities.Apply {FinancialGrade = new FinancialReviewDetails()};
-            var clarificationFiles = new List<ClarificationFile> { new ClarificationFile { Filename = _fileName } };
-            financialGrade.ClarificationFiles = clarificationFiles;
             _repository.Setup(x => x.GetApplication(_applicationId)).ReturnsAsync(application);
-         
             _repository.Setup(x => x.UpdateFinancialReviewDetails(_applicationId, It.Is<FinancialReviewDetails>(g=>g.ClarificationFiles.First().Filename==_fileName))).ReturnsAsync(true);
             var result = _handler.Handle(_request, new CancellationToken()).GetAwaiter().GetResult();
             Assert.IsTrue(result);
@@ -49,14 +40,10 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.ClarificationFileU
 
 
         [Test]
-        public async Task Update_Financial_Grade_with_clarification_Files_name_when_one_file_present()
+        public void Update_Financial_Grade_with_clarification_Files_name_when_one_file_present()
         {
-            var financialGrade = new FinancialReviewDetails();
             var application = new Domain.Entities.Apply { FinancialGrade = new FinancialReviewDetails {ClarificationFiles = new List<ClarificationFile> {new ClarificationFile {Filename = "first.pdf"}}} };
-            var clarificationFiles = new List<ClarificationFile> { new ClarificationFile { Filename = _fileName } };
-            financialGrade.ClarificationFiles = clarificationFiles;
             _repository.Setup(x => x.GetApplication(_applicationId)).ReturnsAsync(application);
-
             _repository.Setup(x => x.UpdateFinancialReviewDetails(_applicationId, It.Is<FinancialReviewDetails>(g => g.ClarificationFiles.Count==2))).ReturnsAsync(true);
             var result = _handler.Handle(_request, new CancellationToken()).GetAwaiter().GetResult();
             Assert.IsTrue(result);
