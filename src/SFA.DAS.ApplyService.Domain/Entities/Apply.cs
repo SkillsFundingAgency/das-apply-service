f7b50c8d92efa351b87980bdb525521b69caf970
Martin Lush <martin-hornsby@ntlworld.com>
2020-12-21 11:45:58 +0000

    APR-2033 - Include new Withdrawn status

--- src/SFA.DAS.ApplyService.Domain/Entities/Apply.cs
+++ src/SFA.DAS.ApplyService.Domain/Entities/Apply.cs
@@ -145,6 +145,7 @@ namespace SFA.DAS.ApplyService.Domain.Entities
         public const string Rejected = "Rejected";	
         public const string Approved = "Approved";	
         public const string Cancelled = "Cancelled";
+        public const string Withdrawn = "Withdrawn";
 
         // Below are other statuses mentioned in the most recent status documentation
         // Please check the flow in RoatpApplicationController, under the line ' switch (application.ApplicationStatus)' if you add new statuses
