bbd4d37adefd5e02a0b6a0796e79e493e4132763
Martin Lush <martin-hornsby@ntlworld.com>
2020-12-24 10:16:03 +0000

    APR-2033 -  Updated with latest fields to show on Dashboards

--- src/SFA.DAS.ApplyService.Domain/Apply/RoatpApplicationSummaryItem.cs
+++ src/SFA.DAS.ApplyService.Domain/Apply/RoatpApplicationSummaryItem.cs
@@ -12,19 +12,28 @@ namespace SFA.DAS.ApplyService.Domain.Apply
         public string Ukprn { get; set; }
         public string ApplicationReferenceNumber { get; set; }
         public DateTime? SubmittedDate { get; set; }
-        public DateTime? FeedbackAddedDate { get; set; }
-        public DateTime? ClosedDate { get; set; }
         public string ApplicationStatus { get; set; }
         public string GatewayReviewStatus { get; set; }
         public string AssessorReviewStatus { get; set; }
         public string FinancialReviewStatus { get; set; }
-        public string GatewayUserName { get; set; }
+        
     }
 
     public class RoatpFinancialSummaryItem : RoatpApplicationSummaryItem
     {
+        public DateTime? FeedbackAddedDate { get; set; }
+        public DateTime? ClosedDate { get; set; }
+
         public string DeclaredInApplication { get; set; }
 
         public FinancialReviewDetails FinancialReviewDetails { get; set; }
     }
- }
+
+    public class RoatpGatewaySummaryItem : RoatpApplicationSummaryItem
+    {
+        public string LastCheckedBy { get; set; }
+        public DateTime? ClarificationRequestedDate { get; set; }
+        public DateTime? OutcomeMadeDate { get; set; }
+        public string OutcomeMadeBy { get; set; }
+    }
+}
