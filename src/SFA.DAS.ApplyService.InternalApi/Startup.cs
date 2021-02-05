9ef0fd53d5b0633b6ee67fa4e5157ed11f4d5a31
chrisfoster186 <chrisfoster186@googlemail.com>
2021-02-05 11:17:00 +0000

    Reworking email service

--- src/SFA.DAS.ApplyService.InternalApi/Startup.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Startup.cs
@@ -315,7 +315,7 @@ namespace SFA.DAS.ApplyService.InternalApi
             });
 
             services.AddTransient<IEmailTokenService, EmailTokenService>();
-            services.AddTransient<IWithdrawApplicationConfirmationEmailService, WithdrawApplicationConfirmationEmailService>();
+            services.AddTransient<IApplicationUpdatedEmailService, ApplicationUpdatedEmailService>();
         }
     }
 }
