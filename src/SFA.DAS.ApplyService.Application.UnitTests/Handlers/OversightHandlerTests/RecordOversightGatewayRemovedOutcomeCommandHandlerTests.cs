d7367f4269c7629b76cea07c02c45ef8e5666a16
chrisfoster186 <chrisfoster186@googlemail.com>
2021-02-10 10:56:30 +0000

    Send notification

--- src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/OversightHandlerTests/RecordOversightGatewayRemovedOutcomeCommandHandlerTests.cs
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/OversightHandlerTests/RecordOversightGatewayRemovedOutcomeCommandHandlerTests.cs
@@ -8,6 +8,7 @@ using SFA.DAS.ApplyService.Application.Apply.Oversight;
 using SFA.DAS.ApplyService.Application.Interfaces;
 using SFA.DAS.ApplyService.Domain.Entities;
 using SFA.DAS.ApplyService.Domain.Interfaces;
+using SFA.DAS.ApplyService.EmailService.Interfaces;
 using SFA.DAS.ApplyService.Types;
 
 namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.OversightHandlerTests
@@ -19,6 +20,7 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.OversightHandlerTe
         private Mock<IApplyRepository> _applyRepository;
         private Mock<IOversightReviewRepository> _oversightReviewRepository;
         private Mock<IAuditService> _auditService;
+        private Mock<IApplicationUpdatedEmailService> _applicationUpdatedEmailService;
         private Guid _applicationId;
 
         [SetUp]
@@ -28,6 +30,7 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.OversightHandlerTe
             _applyRepository = new Mock<IApplyRepository>();
             _oversightReviewRepository = new Mock<IOversightReviewRepository>();
             _auditService = new Mock<IAuditService>();
+            _applicationUpdatedEmailService = new Mock<IApplicationUpdatedEmailService>();
 
             _applyRepository.Setup(x => x.GetApplication(_applicationId)).ReturnsAsync(() => new Domain.Entities.Apply
                 { ApplicationId = _applicationId, Status = ApplicationStatus.Submitted });
@@ -37,10 +40,13 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.OversightHandlerTe
 
             _auditService.Setup(x => x.AuditInsert(It.IsAny<OversightReview>()));
 
+            _applicationUpdatedEmailService.Setup(x => x.SendEmail(_applicationId)).Returns(Task.CompletedTask);
+
             _handler = new RecordOversightGatewayRemovedOutcomeCommandHandler(_applyRepository.Object,
                 _oversightReviewRepository.Object,
                 Mock.Of<ILogger<RecordOversightGatewayRemovedOutcomeCommandHandler>>(),
-                _auditService.Object);
+                _auditService.Object,
+                _applicationUpdatedEmailService.Object);
         }
 
         [Test]
@@ -79,6 +85,21 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Handlers.OversightHandlerTe
                 r.Status == OversightReviewStatus.Removed &&
                 r.UserId == request.UserId &&
                 r.UserName == request.UserName)));
+        }
+
+        [Test]
+        public async Task Handle_User_Is_Notified_Of_Update_To_Application()
+        {
+            var request = new RecordOversightGatewayRemovedOutcomeCommand
+            {
+                ApplicationId = _applicationId,
+                UserId = "test user id",
+                UserName = "test user name"
+            };
+
+            await _handler.Handle(request, new CancellationToken());
+
+            _applicationUpdatedEmailService.Verify(x => x.SendEmail(_applicationId), Times.Once);
         }
     }
 }
--- src/SFA.DAS.ApplyService.Application/Apply/Oversight/RecordOversightGatewayRemovedOutcomeCommandHandler.cs
+++ src/SFA.DAS.ApplyService.Application/Apply/Oversight/RecordOversightGatewayRemovedOutcomeCommandHandler.cs
@@ -7,6 +7,7 @@ using SFA.DAS.ApplyService.Application.Interfaces;
 using SFA.DAS.ApplyService.Domain.Audit;
 using SFA.DAS.ApplyService.Domain.Entities;
 using SFA.DAS.ApplyService.Domain.Interfaces;
+using SFA.DAS.ApplyService.EmailService.Interfaces;
 using SFA.DAS.ApplyService.Types;
 
 namespace SFA.DAS.ApplyService.Application.Apply.Oversight
@@ -17,21 +18,24 @@ namespace SFA.DAS.ApplyService.Application.Apply.Oversight
         private readonly IOversightReviewRepository _oversightReviewRepository;
         private readonly ILogger<RecordOversightGatewayRemovedOutcomeCommandHandler> _logger;
         private readonly IAuditService _auditService;
+        private readonly IApplicationUpdatedEmailService _applicationUpdatedEmailService;
 
         public RecordOversightGatewayRemovedOutcomeCommandHandler(IApplyRepository applyRepository,
             IOversightReviewRepository oversightReviewRepository,
             ILogger<RecordOversightGatewayRemovedOutcomeCommandHandler> logger,
-            IAuditService auditService)
+            IAuditService auditService,
+            IApplicationUpdatedEmailService applicationUpdatedEmailService)
         {
             _applyRepository = applyRepository;
             _oversightReviewRepository = oversightReviewRepository;
             _logger = logger;
             _auditService = auditService;
+            _applicationUpdatedEmailService = applicationUpdatedEmailService;
         }
 
         public async Task<Unit> Handle(RecordOversightGatewayRemovedOutcomeCommand request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation($"Recording Oversight review status of Unsuccessful for gateway fail application Id {request.ApplicationId}");
+            _logger.LogInformation($"Recording Oversight review status of Unsuccessful for gateway removed application Id {request.ApplicationId}");
 
             var application = await _applyRepository.GetApplication(request.ApplicationId);
 
@@ -54,6 +58,8 @@ namespace SFA.DAS.ApplyService.Application.Apply.Oversight
             await _oversightReviewRepository.Add(oversightReview);
            
             await _auditService.Save();
+
+            await _applicationUpdatedEmailService.SendEmail(request.ApplicationId);
 
             return Unit.Value;
         }
