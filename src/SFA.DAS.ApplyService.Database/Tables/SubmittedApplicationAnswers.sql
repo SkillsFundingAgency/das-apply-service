af0ff73eb1c9b8e9c586d3520631a62ba55d9aa2
Martin Lush <martin-hornsby@ntlworld.com>
2021-03-11 11:04:26 +0000

    APR-2223 - Added new tables for application extract function

---
+++ src/SFA.DAS.ApplyService.Database/Tables/SubmittedApplicationAnswers.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[SubmittedApplicationAnswers]
+(
+	[Id] INT NOT NULL IDENTITY(1,1), 
+    [ApplicationId] UNIQUEIDENTIFIER NOT NULL,  
+    [PageId] NVARCHAR(10) NOT NULL,
+	[QuestionId] NVARCHAR(10) NOT NULL,
+	[QuestionType] NVARCHAR(10) NOT NULL,
+	[Answer] NVARCHAR(MAX) NULL,
+	[ColumnHeading] NVARCHAR(100) NULL,
+
+	CONSTRAINT PK_SubmittedApplicationAnswers PRIMARY KEY (Id),
+	INDEX IX_SubmittedApplicationAnswers_ByApplication (ApplicationId)
+)
+GO
