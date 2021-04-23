669b0be3bdaef8c1a8e6ec834245bea2eca2b55c
Martin Lush <martin-hornsby@ntlworld.com>
2021-04-23 11:36:29 +0100

    APR-2389 - Keep a record of when QnA files have been extracted into Datamart

--- src/SFA.DAS.ApplyService.Database/Tables/ExtractedApplications.sql
+++ src/SFA.DAS.ApplyService.Database/Tables/ExtractedApplications.sql
@@ -3,6 +3,7 @@
 	[Id] INT NOT NULL IDENTITY(1,1), 
     [ApplicationId] UNIQUEIDENTIFIER NOT NULL,  
     [ExtractedDate] DATETIME2 NOT NULL,
+    [QnaFilesExtracted] BIT NOT NULL,
 
     CONSTRAINT PK_ExtractedApplications PRIMARY KEY (Id),
     INDEX IX_ExtractedApplications_ApplicationId (ApplicationId)
