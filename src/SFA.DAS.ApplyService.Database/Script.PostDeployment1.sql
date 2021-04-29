f0978362a67aff83566b5b07782d6a81e6c7ba21
Martin Lush <martin-hornsby@ntlworld.com>
2021-04-29 08:38:30 +0100

    APR-2441 - Ensure EmailTemplates are deleted before inserting new ones

--- src/SFA.DAS.ApplyService.Database/Script.PostDeployment1.sql
+++ src/SFA.DAS.ApplyService.Database/Script.PostDeployment1.sql
@@ -11,17 +11,19 @@ Post-Deployment Script Template
 */
 
 -- cleanup (for now)
-DELETE FROM [EmailTemplates]
+BEGIN
+	DELETE FROM EmailTemplates;
+END
 
 IF NOT EXISTS (SELECT * FROM EmailTemplates WHERE TemplateName = N'RoATPGetHelpWithQuestion')
 BEGIN
-	INSERT INTO EmailTemplates ([Id], [Status],[TemplateName],[TemplateId],[Recipients],[CreatedAt],[CreatedBy]) 
+	INSERT INTO EmailTemplates ([Id], [Status], [TemplateName], [TemplateId], [Recipients], [CreatedAt], [CreatedBy]) 
 	VALUES (NEWID(), 'Live', N'RoATPGetHelpWithQuestion', N'9d1e1a7e-3557-4781-8901-ea627ae70ec2', N'RoATP.SUPPORT@education.gov.uk', GETDATE(), 'System')
 END
 
 IF NOT EXISTS (SELECT * FROM EmailTemplates WHERE TemplateName = N'RoATPApplicationSubmitted')
 BEGIN
-	INSERT INTO EmailTemplates ([Id], [Status],[TemplateName],[TemplateId],[CreatedAt],[CreatedBy]) 
+	INSERT INTO EmailTemplates ([Id], [Status], [TemplateName], [TemplateId], [CreatedAt], [CreatedBy]) 
 	VALUES (NEWID(), 'Live', N'RoATPApplicationSubmitted', N'4a44e79d-1e98-4b90-9d67-f575be97def6', GETDATE(), 'System')
 END
 
@@ -32,7 +34,7 @@ END
 
 IF NOT EXISTS (SELECT * FROM EmailTemplates WHERE TemplateName = N'RoATPApplicationUpdated')
 BEGIN
-	INSERT INTO EmailTemplates ([Id], [Status],[TemplateName],[TemplateId],[CreatedAt],[CreatedBy]) 
+	INSERT INTO EmailTemplates ([Id], [Status], [TemplateName], [TemplateId], [CreatedAt], [CreatedBy]) 
 	VALUES (NEWID(), 'Live', N'RoATPApplicationUpdated', N'ebb28424-b1ce-4374-b24b-a240f0cecdc1', GETDATE(), 'System')
 END
 
