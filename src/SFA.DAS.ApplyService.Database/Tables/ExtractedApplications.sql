f76f91c6667cf50ab742124239c67561813938b4
Martin Lush <martin-hornsby@ntlworld.com>
2021-05-27 08:08:19 +0100

    APR-2389 - Qna files are now extracted via Service Bus messages

--- src/SFA.DAS.ApplyService.Database/Tables/ExtractedApplications.sql
+++ src/SFA.DAS.ApplyService.Database/Tables/ExtractedApplications.sql
@@ -3,7 +3,6 @@
 	[Id] INT NOT NULL IDENTITY(1,1), 
     [ApplicationId] UNIQUEIDENTIFIER NOT NULL,  
     [ExtractedDate] DATETIME2 NOT NULL,
-    [QnaFilesExtracted] BIT NOT NULL,
 
     CONSTRAINT PK_ExtractedApplications PRIMARY KEY (Id),
     INDEX IX_ExtractedApplications_ApplicationId (ApplicationId)
