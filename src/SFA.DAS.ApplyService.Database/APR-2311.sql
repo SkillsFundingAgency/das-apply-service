0b96c17567bbe003d058f707c66ef5b5e490c2b5
MarkFCain <mark.cain@digital.education.gov.uk>
2021-07-26 11:53:16 +0100

    APR-2311 data structures and hydration scripts built

---
+++ src/SFA.DAS.ApplyService.Database/APR-2311.sql
@@ -0,0 +1,110 @@
+﻿--APR-2311 convert FinancialGrade to separate tables
+INSERT INTO [dbo].[FinancialReview]
+           (
+           [ApplicationId]
+           ,[Status]
+           ,[SelectedGrade]
+		   ,[FinancialDueDate]
+           ,[GradedBy]
+           ,[GradedOn]
+           ,[Comments]
+           ,[ExternalComments]
+           ,[ClarificationRequestedOn]
+           ,[ClarificationRequestedBy]
+           ,[ClarificationResponse])
+SELECT 
+    [ApplicationId],
+    [FinancialReviewStatus] as Status,                  
+    JSON_VALUE(apply.FinancialGrade, '$.SelectedGrade') AS SelectedGrade,
+	JSON_VALUE(apply.FinancialGrade, '$.FinancialDueDate') AS FinancialDueDate,
+		JSON_VALUE(apply.FinancialGrade, '$.GradedBy') AS GradedBy,	
+		JSON_VALUE(apply.FinancialGrade, '$.GradedDateTime') AS GradedOn,	
+		JSON_VALUE(apply.FinancialGrade, '$.Comments') AS Comments,	
+		JSON_VALUE(apply.FinancialGrade, '$.ExternalComments') AS ExternalComments,	
+		JSON_VALUE(apply.FinancialGrade, '$.ClarificationRequestedOn') AS ClarificationRequestedOn,		
+		JSON_VALUE(apply.FinancialGrade, '$.ClarificationRequestedBy') AS ClarificationRequestedBy,		
+		JSON_VALUE(apply.FinancialGrade, '$.ClarificationResponse') AS ClarificationResponse
+  FROM [dbo].[Apply] apply
+  where applicationID not in (select applicationId from FinancialReview)
+  and FinancialGrade is not null
+
+
+
+insert into FinancialReviewEvidenceFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[0].Filename') AS FinancialEvidenceFilename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[0].Filename') not in (select filename from FinancialReviewEvidenceFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[0].Filename') is not null
+
+
+insert into FinancialReviewEvidenceFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[1].Filename') AS FinancialEvidenceFilename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[1].Filename') not in (select filename from FinancialReviewEvidenceFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[1].Filename') is not null
+
+
+insert into FinancialReviewEvidenceFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[2].Filename') AS FinancialEvidenceFilename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[2].Filename') not in (select filename from FinancialReviewEvidenceFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[2].Filename') is not null
+
+ 
+ 
+ insert into FinancialReviewEvidenceFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[3].Filename') AS FinancialEvidenceFilename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[3].Filename') not in (select filename from FinancialReviewEvidenceFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.FinancialEvidences[3].Filename') is not null
+
+insert into FinancialReviewClarificationFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[0].Filename') AS Filename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[0].Filename') not in (select filename from FinancialReviewClarificationFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[0].Filename') is not null
+
+
+insert into FinancialReviewClarificationFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[1].Filename') AS Filename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[1].Filename') not in (select filename from FinancialReviewClarificationFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[1].Filename') is not null
+
+insert into FinancialReviewClarificationFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[2].Filename') AS Filename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[2].Filename') not in (select filename from FinancialReviewClarificationFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[2].Filename') is not null
+
+insert into FinancialReviewClarificationFile (applicationId, Filename)
+SELECT 
+    [ApplicationId],
+		JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[3].Filename') AS Filename  
+  FROM [dbo].[Apply] apply
+  WHERE JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[3].Filename') not in (select filename from FinancialReviewClarificationFile)
+  AND FinancialGrade is not null
+  AND JSON_VALUE(apply.FinancialGrade, '$.ClarificationFiles[3].Filename') is not null
+
+-- END OF APR-2311
---
+++ src/SFA.DAS.ApplyService.Database/Tables/FinancialReview.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[FinancialReview]
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY CLUSTERED DEFAULT NEWID(),
+	[ApplicationId] [uniqueidentifier] NOT NULL,
+	[Status] nvarchar(20)  NOT NULL,
+	[SelectedGrade] [nvarchar](20) NULL,
+	[FinancialDueDate] [datetime2](7) NULL,
+	[GradedBy] [nvarchar](256) NULL,
+	[GradedOn] [datetime2](7) NULL,
+	[Comments] [nvarchar](max) NULL,
+	[ExternalComments] [nvarchar](max) NULL,
+	[ClarificationRequestedOn] [datetime2](7) NULL,
+	[ClarificationRequestedBy] [nvarchar](256) NULL,
+	[ClarificationResponse] [nvarchar](max) NULL
+)
+
+GO
+
+CREATE INDEX [IX_FinancialReview_ApplicationId] ON [dbo].[FinancialReview] (ApplicationId)
