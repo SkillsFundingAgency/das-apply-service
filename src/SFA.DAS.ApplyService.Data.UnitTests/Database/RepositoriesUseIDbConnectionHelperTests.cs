c09f92d697deb4c984c83a6055eaa29d11e39eee
Martin Lush <martin-hornsby@ntlworld.com>
2021-08-05 10:50:48 +0100

    APR-2639 - Update IDbConnectionHelper checks to reflect latest set of repositories

--- src/SFA.DAS.ApplyService.Data.UnitTests/Database/RepositoriesUseIDbConnectionHelperTests.cs
+++ src/SFA.DAS.ApplyService.Data.UnitTests/Database/RepositoriesUseIDbConnectionHelperTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using NUnit.Framework;
@@ -11,14 +12,19 @@ namespace SFA.DAS.ApplyService.Data.UnitTests.Database
     {
         private readonly Type IDbConnectionHelperType = typeof(IDbConnectionHelper);
 
+        private readonly List<string> _repositoriesThatDoNotRequireIDbConnectionHelper = new List<string>()
+        {
+            "AuditRepository"
+        };
+
         [Test]
         public void Repositories_Must_Use_IDbConnectionHelper()
         {
             var webAssembly = typeof(ApplyRepository).GetTypeInfo().Assembly;
 
             var repositories = webAssembly.DefinedTypes.Where(c => c.Name.EndsWith("Repository")).ToList();
-       
-            foreach (var repository in repositories)
+
+            foreach (var repository in repositories.Where(repo => !_repositoriesThatDoNotRequireIDbConnectionHelper.Contains(repo.Name)))
             {
                 var constructorWithIDbConnectionHelper = repository.GetConstructor(BindingFlags.Instance | BindingFlags.Public, null, CallingConventions.HasThis, new[] { IDbConnectionHelperType }, null);
 
---
+++ src/SFA.DAS.ApplyService.Data.UnitTests/Database/UnitOfWorkUsesIDbConnectionHelperTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using NUnit.Framework;
+using SFA.DAS.ApplyService.Infrastructure.Database;
+
+namespace SFA.DAS.ApplyService.Data.UnitTests.Database
+{
+    [TestFixture]
+    public class UnitOfWorkUsesIDbConnectionHelperTest
+    {
+        private readonly Type IDbConnectionHelperType = typeof(IDbConnectionHelper);
+
+        [Test]
+        public void UnitOfWork_Must_Use_IDbConnectionHelper()
+        {
+            var unitOfWorkType = typeof(UnitOfWork.UnitOfWork);
+
+            var constructorWithIDbConnectionHelper = unitOfWorkType.GetConstructor(BindingFlags.Instance | BindingFlags.Public, null, CallingConventions.HasThis, new[] { IDbConnectionHelperType }, null);
+
+            Assert.IsNotNull(constructorWithIDbConnectionHelper, $"{unitOfWorkType.FullName} does not use {IDbConnectionHelperType.FullName}");
+        }
+    }
+}
