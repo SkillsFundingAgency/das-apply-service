9cc97aff6362e5bd6a8ee3a5194202d4a48d925a
MarkFCain <mark.cain@digital.education.gov.uk>
2021-08-19 14:16:09 +0100

    APR-2570 updated tests

--- src/SFA.DAS.ApplyService.Application.UnitTests/Services/BankHolidayServiceTests.cs
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Services/BankHolidayServiceTests.cs
@@ -8,14 +8,18 @@ namespace SFA.DAS.ApplyService.Application.UnitTests.Services
     [TestFixture]
     public class BankHolidayServiceTests
     {
-        [TestCase("2020-01-01",10,"2020-01-15")]
+        [TestCase("2020-01-01", 10, "2020-01-15")]
         [TestCase("2021-02-01", 10, "2021-02-15")]
         [TestCase("2021-03-01", 15, "2021-03-22")]
-        [TestCase("2021-04-01", 20, "2021-04-29")]
-        [TestCase("2021-05-01", 30, "2021-06-14")]
+        [TestCase("2021-04-01", 20, "2021-05-04")]
+        [TestCase("2021-05-01", 30, "2021-06-15")]
         [TestCase("2021-06-01", 10, "2021-06-15")]
         [TestCase("2021-07-01", 10, "2021-07-15")]
-        [TestCase("2021-08-01", 10, "2021-08-16")]
+        [TestCase("2021-08-01", 10, "2021-08-13")]
+        [TestCase("2021-08-13", 10, "2021-08-27")]
+        [TestCase("2021-08-14", 10, "2021-08-27")]
+        [TestCase("2021-08-15", 10, "2021-08-27")]
+        [TestCase("2021-08-16", 10, "2021-08-31")]
         public void GetWorkingDaysAhead_returns_expectedDate(DateTime startDate,int daysAhead,DateTime? expectedDate)
         {
             var service = new BankHolidayService();
