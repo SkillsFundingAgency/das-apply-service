ba257327de5c305e982f704b917556d126737c25
Martin Lush <martin-hornsby@ntlworld.com>
2021-08-24 11:53:04 +0100

    APR-2640 - Updated unit tests in InternalAPI to handle Appeal files

--- src/SFA.DAS.ApplyService.InternalApi.UnitTests/OversightControllerTests.cs
+++ src/SFA.DAS.ApplyService.InternalApi.UnitTests/OversightControllerTests.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture;
 using FluentAssertions;
 using KellermanSoftware.CompareNetObjects;
 using MediatR;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Internal;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ApplyService.Application.Apply.Oversight;
-using SFA.DAS.ApplyService.Application.Apply.Oversight.Commands.UploadAppealFile;
-using SFA.DAS.ApplyService.Application.Apply.Oversight.Queries.GetAppealUpload;
 using SFA.DAS.ApplyService.Application.Apply.Oversight.Queries.GetOversightDetails;
 using SFA.DAS.ApplyService.Application.Apply.Oversight.Queries.GetOversightReview;
-using SFA.DAS.ApplyService.Application.Apply.Oversight.Queries.GetStagedFiles;
 using SFA.DAS.ApplyService.Domain.QueryResults;
 using SFA.DAS.ApplyService.InternalApi.Controllers;
 using SFA.DAS.ApplyService.InternalApi.Services;
@@ -265,139 +258,6 @@ namespace SFA.DAS.ApplyService.InternalApi.UnitTests
             result.Value.UKPRN.Should().Be(response.UKPRN);
         }
 
-        // TODO: APPEALREVIEW - Review once appeal work starts
-        // commented out as might lift it to other controller
-    //     [Test]
-    //     public async Task Upload_Appeal_File_Adds_Uploaded_File_To_Application()
-    //     {
-    //         var request = new UploadAppealFileRequest
-    //         {
-    //             ApplicationId = AutoFixture.Create<Guid>(),
-    //             File = GenerateFile(),
-    //             UserId = AutoFixture.Create<string>(),
-    //             UserName = AutoFixture.Create<string>()
-    //         };
-    //
-    //         _mediator.Setup(x => x.Send(It.IsAny<UploadAppealFileCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(Unit.Value);
-    //
-    //         var result = await _controller.UploadAppealFile(request);
-    //         result.Should().BeOfType<OkResult>();
-    //
-    //         string expectedFileData;
-    //         using (var reader = new StreamReader(request.File.OpenReadStream()))
-    //         {
-    //             expectedFileData = await reader.ReadToEndAsync();
-    //         }
-    //
-    //         _mediator.Verify(x => x.Send(It.Is<UploadAppealFileCommand>(c =>
-    //                 c.ApplicationId == request.ApplicationId
-    //                 && c.UserId == request.UserId
-    //                 && c.UserName == request.UserName
-    //                 && c.File.Filename == request.File.FileName
-    //                 && Encoding.UTF8.GetString(c.File.Data) == expectedFileData),
-    //             It.IsAny<CancellationToken>()), Times.Once);
-    //     }
-    //
-    //     [Test]
-    //     public async Task Remove_Appeal_File_Removes_Uploaded_File_From_Application()
-    //     {
-    //         var applicationId = AutoFixture.Create<Guid>();
-    //         var fileId = AutoFixture.Create<Guid>();
-    //
-    //         var request = new RemoveAppealFileRequest
-    //         {
-    //             UserId = AutoFixture.Create<string>(),
-    //             UserName = AutoFixture.Create<string>()
-    //         };
-    //
-    //         _mediator.Setup(x => x.Send(It.IsAny<RemoveAppealFileCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(Unit.Value);
-    //
-    //         var result = await _controller.RemoveAppealFile(applicationId, fileId, request);
-    //         result.Should().BeOfType<OkResult>();
-    //
-    //         _mediator.Verify(
-    //             x => x.Send(It.Is<RemoveAppealFileCommand>(c =>
-    //                 c.ApplicationId == applicationId
-    //                 && c.FileId == fileId
-    //                 && c.UserId == request.UserId
-    //                 && c.UserName == request.UserName), 
-    // It.IsAny<CancellationToken>()), Times.Once);
-    //     }
-    //
-    //     [Test]
-    //     public async Task AppealUploads_Gets_Files_For_Application_Appeal()
-    //     {
-    //         var request = new GetStagedFilesRequest();
-    //         var queryResult = new GetStagedFilesQueryResult
-    //         {
-    //             Files = new List<GetStagedFilesQueryResult.AppealFile>
-    //             {
-    //                 new GetStagedFilesQueryResult.AppealFile{ Id = Guid.NewGuid(), Filename = AutoFixture.Create<string>()}
-    //             }
-    //         };
-    //
-    //         _mediator.Setup(x => x.Send(It.IsAny<GetStagedFilesQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(queryResult);
-    //
-    //         var result = await _controller.StagedUploads(request);
-    //         result.Should().BeOfType<ActionResult<GetStagedFilesResponse>>();
-    //
-    //         var compareLogic = new CompareLogic(new ComparisonConfig { IgnoreObjectTypes = true });
-    //         var comparisonResult = compareLogic.Compare(queryResult, result);
-    //         Assert.IsTrue(comparisonResult.AreEqual);
-    //     }
-    //
-    //     [Test]
-    //     public async Task CreateAppeal_Adds_Appeal_To_Oversight_Review()
-    //     {
-    //         var applicationId = AutoFixture.Create<Guid>();
-    //         var oversightReviewId = AutoFixture.Create<Guid>();
-    //         var request = AutoFixture.Create<CreateAppealRequest>();
-    //
-    //         _mediator.Setup(x => x.Send(It.IsAny<CreateAppealCommand>(), It.IsAny<CancellationToken>())).ReturnsAsync(Unit.Value);
-    //
-    //         var result = await _controller.CreateAppeal(applicationId, oversightReviewId, request);
-    //         Assert.IsInstanceOf<OkResult>(result);
-    //
-    //         _mediator.Verify(x => x.Send(It.Is<CreateAppealCommand>(c =>
-    //             c.ApplicationId == applicationId &&
-    //             c.OversightReviewId == oversightReviewId &&
-    //             c.Message == request.Message &&
-    //             c.UserId == request.UserId &&
-    //             c.UserName == request.UserName), It.IsAny<CancellationToken>()));
-    //     }
-    //
-    //     [Test]
-    //     public async Task GetAppeal_Gets_Appeal_For_Application_And_OversightReview()
-    //     {
-    //         var request = new GetAppealRequest();
-    //         var queryResult = AutoFixture.Create<GetAppealQueryResult>();
-    //
-    //         _mediator.Setup(x => x.Send(It.IsAny<GetAppealQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(queryResult);
-    //
-    //         var result = await _controller.GetAppeal(request);
-    //         result.Should().BeOfType<ActionResult<GetAppealResponse>>();
-    //
-    //         var compareLogic = new CompareLogic(new ComparisonConfig { IgnoreObjectTypes = true });
-    //         var comparisonResult = compareLogic.Compare(queryResult, result);
-    //         Assert.IsTrue(comparisonResult.AreEqual);
-    //     }
-    //
-    //     [Test]
-    //     public async Task GetAppealUpload_Gets_AppealUpload_For_Application_Appeal()
-    //     {
-    //         var request = new GetAppealUploadRequest();
-    //         var queryResult = AutoFixture.Create<GetAppealUploadQueryResult>();
-    //
-    //         _mediator.Setup(x => x.Send(It.IsAny<GetAppealUploadQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(queryResult);
-    //
-    //         var result = await _controller.GetAppealUpload(request);
-    //         result.Should().BeOfType<ActionResult<GetAppealUploadResponse>>();
-    //
-    //         Assert.AreEqual(queryResult.Filename, result.Value.Filename);
-    //         Assert.AreEqual(queryResult.Content, result.Value.Content);
-    //         Assert.AreEqual(queryResult.ContentType, result.Value.ContentType);
-    //     }
-
         [Test]
         public async Task OversightReview_Gets_OversightReview_For_Application()
         {
@@ -412,21 +272,6 @@ namespace SFA.DAS.ApplyService.InternalApi.UnitTests
             var compareLogic = new CompareLogic(new ComparisonConfig { IgnoreObjectTypes = true });
             var comparisonResult = compareLogic.Compare(queryResult, result);
             Assert.IsTrue(comparisonResult.AreEqual);
-        }
-
-        private static IFormFile GenerateFile()
-        {
-            var fileName = AutoFixture.Create<string>();
-            var content = AutoFixture.Create<string>();
-            return new FormFile(new MemoryStream(Encoding.UTF8.GetBytes(content)),
-                0,
-                content.Length,
-                fileName,
-                fileName)
-            {
-                Headers = new HeaderDictionary(),
-                ContentType = "application/pdf"
-            };
         }
     }
 }
