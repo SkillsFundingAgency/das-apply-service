5bbbe061e5e258097530061f4254e80667acdedd
MarkFCain <mark.cain@digital.education.gov.uk>
2021-09-13 10:20:39 +0100

    APR-2650 minor handler fix

--- src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/Appeals/RecordAppealOutcomeHandlerTests.cs
+++ src/SFA.DAS.ApplyService.Application.UnitTests/Handlers/Appeals/RecordAppealOutcomeHandlerTests.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using FluentAssertions;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
-using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
