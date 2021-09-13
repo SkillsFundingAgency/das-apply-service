84126d280cb576a38617e9501c9785768aef4813
Martin Lush <martin-hornsby@ntlworld.com>
2021-09-13 10:26:43 +0100

    APR-2689 - Adding new API types from Charities Commission

---
+++ src/SFA.DAS.ApplyService.InternalApi/Infrastructure/CharityCommission/Entities/Classification.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace SFA.DAS.ApplyService.InternalApi.Infrastructure.CharityCommission.Entities
+{
+    public enum ClassificationType
+    {
+        What,
+        Who,
+        How
+    }
+
+    public class Classification
+    {
+        [JsonProperty("classification_code")]
+        public string Code { get; set; }
+
+        [JsonProperty("classification_type")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ClassificationType Type { get; set; }
+
+        [JsonProperty("classification_desc")]
+        public string Description { get; set; }
+    }
+}
---
+++ src/SFA.DAS.ApplyService.InternalApi/Infrastructure/CharityCommission/Entities/Trustee.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace SFA.DAS.ApplyService.InternalApi.Infrastructure.CharityCommission.Entities
+{
+    public class Trustee
+    {
+        [JsonProperty("organisation_number")]
+        public long OrganisationNumber { get; set; }
+
+        [JsonProperty("trustee_name")]
+        public string Name { get; set; }
+    }
+}
