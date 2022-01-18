8b780e45ed2682d389c2cce01f30885cec332d73
MarkFCain <31033361+MarkFCain@users.noreply.github.com>
2022-01-18 14:07:10 +0000

    APR-2775 added security header and changed startup to suit story (#942)

--- src/SFA.DAS.ApplyService.Web/Startup.cs
+++ src/SFA.DAS.ApplyService.Web/Startup.cs
@@ -311,12 +311,12 @@ namespace SFA.DAS.ApplyService.Web
                 app.UseHttpsRedirection();
             }
             
-            app.UseStaticFiles();
             app.UseSession();
-            app.UseAuthentication();
             app.UseRequestLocalization();
             app.UseStatusCodePagesWithReExecute("/Home/error/{0}");
             app.UseSecurityHeaders();
+            app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseHealthChecks("/health");
             app.UseMvc(routes =>
             {
