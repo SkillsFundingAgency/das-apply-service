2f54cdd38709e1f9b5aa5e76305f23f18deb020a
Deven Shah <devenshah1974@gmail.com>
2022-01-20 13:36:39 +0000

    APR-2930 Table structure for data enhancements (#944)
    * APR-2930 Table structure for data enhancements
    
    * APR-2930 Removed refactor log from project file.

---
+++ src/SFA.DAS.ApplyService.Database/Tables/OrganisationManagement.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[OrganisationManagement]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[OrganisationId] UNIQUEIDENTIFIER NOT NULL,
+	[FirstName] NVARCHAR(300) NOT NULL,
+	[LastName] NVARCHAR(300) NOT NULL,
+	[JobRole] NVARCHAR(300) NOT NULL,
+	[TimeInRoleMonths] [INT] NOT NULL,
+	[IsPartOfAnyOtherOrganisation] [BIT] NOT NULL,
+	[OtherOrganisationNames] NVARCHAR(1000) NULL,
+	[DateOfBirthMonth] TiNYINT NOT NULL,
+	[DateOfBirthYear] INT NOT NULL,
+	[ContactNumber] NVARCHAR(20) NOT NULL,
+	[Email] NVARCHAR(350) NOT NULL,
+
+	CONSTRAINT PK_OrganisationManagement PRIMARY KEY (Id),
+	CONSTRAINT FK_OrganisationManagement_Organisation FOREIGN KEY (OrganisationId) REFERENCES Organisations(Id)
+)
---
+++ src/SFA.DAS.ApplyService.Database/Tables/OrganisationSectorExpertDeliveredTrainingTypes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[OrganisationSectorExpertDeliveredTrainingTypes]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[OrganisationSectorExpertId] INT NOT NULL,
+	[DeliveredTrainingType] NVARCHAR(300) NOT NULL, 
+
+	CONSTRAINT PK_OrganisationSectorExpertDeliveredTrainingTypes PRIMARY KEY (Id),
+	CONSTRAINT FK_OrganisationSectorExpertDeliveredTrainingTypes_OrganisationSectorExperts FOREIGN KEY (OrganisationSectorExpertId) REFERENCES OrganisationSectorExperts(Id)
+)
