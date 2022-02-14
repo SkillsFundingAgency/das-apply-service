867fda5c4d689bf83fd6a0d0baef88e8f519a816
MarkFCain <mark.cain@digital.education.gov.uk>
2022-02-14 15:26:39 +0000

    APR-2952 gateway service unavailable when ukrlp details not retrieved

--- src/SFA.DAS.ApplyService.InternalApi/Services/GatewayApiChecksService.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Services/GatewayApiChecksService.cs
@@ -11,6 +11,12 @@ using System;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Polly;
+using Polly.Retry;
+using SFA.DAS.ApplyService.InternalApi.Types;
+using SFA.DAS.ApplyService.InternalApi.Types.CharityCommission;
+
+//MFCMFC using ProviderDetails = SFA.DAS.ApplyService.Domain.Ukrlp.ProviderDetails;
 
 namespace SFA.DAS.ApplyService.InternalApi.Services
 {
@@ -21,6 +27,7 @@ namespace SFA.DAS.ApplyService.InternalApi.Services
         private readonly IRoatpApiClient _roatpApiClient;
         private readonly IInternalQnaApiClient _qnaApiClient;
         private readonly ILogger<GatewayApiChecksService> _logger;
+        private readonly AsyncRetryPolicy _retryPolicy;
 
         public GatewayApiChecksService(
             CompaniesHouseApiClient companiesHouseApiClient, 
@@ -34,6 +41,7 @@ namespace SFA.DAS.ApplyService.InternalApi.Services
             _roatpApiClient = roatpApiClient;
             _qnaApiClient = qnaApiClient;
             _logger = logger;
+            _retryPolicy = GetRetryPolicy();
         }
 
         public async Task<ApplyGatewayDetails> GetExternalApiCheckDetails(Guid applicationId)
@@ -82,8 +90,22 @@ namespace SFA.DAS.ApplyService.InternalApi.Services
 
         private async Task LookupCompaniesHouseDetails(ApplyGatewayDetails applyGatewayDetails, string companyNumber)
         {
-            var companyDetails = await _companiesHouseApiClient.GetCompany(companyNumber);
-            if (companyDetails == null)
+            ApiResponse<Types.CompaniesHouse.Company> companyDetails = null;
+            try
+            { 
+                companyDetails =
+                    await _retryPolicy.ExecuteAsync(context => _companiesHouseApiClient.GetCompany(companyNumber),
+                        new Context());
+
+            }
+            catch(Exception ex)
+            {
+                var message = $"Unable to retrieve results from companies house for company number {companyNumber}";
+                _logger.LogError(message, ex);
+                throw new ServiceUnavailableException(message);
+            }
+
+            if (string.IsNullOrEmpty(companyDetails?.Response?.CompanyName))
             {
                 var message = $"Unable to retrieve Companies House details for company number {companyNumber}";
                 _logger.LogError(message);
@@ -95,22 +117,42 @@ namespace SFA.DAS.ApplyService.InternalApi.Services
         private async Task LookupCharityCommissionDetails(ApplyGatewayDetails applyGatewayDetails, string charityNumberFromUkrlp)
         {
             if (!int.TryParse(charityNumberFromUkrlp, out var charityNumber))
-            { 
-                _logger.LogError("Unable to parse charity registration number from charity number in ukrlp `{charityNumber}`", charityNumberFromUkrlp);
-                return;
+            {
+                var message =
+                    $"Unable to parse charity registration number from charity number in ukrlp: '{charityNumberFromUkrlp}'";
+                _logger.LogError(message);
+                throw new ServiceUnavailableException(message);
             }
 
+            Charity charityDetails = null;
             try
             {
-                var charityDetails = await _outerApiClient.GetCharityDetails(charityNumber);
+                charityDetails = await _retryPolicy.ExecuteAsync(context => _outerApiClient.GetCharityDetails(charityNumber), new Context());
                 applyGatewayDetails.CharityCommissionDetails = Mapper.Map<CharityCommissionSummary>(charityDetails);
             }
             catch (HttpRequestException ex)
             {
-                var message = $"Unable to retrieve Charity Commission details for charity number {charityNumber}";
+                var message = $"Unable to retrieve Charity Commission details for charity number {charityNumber} based on charity number from uklrp: {charityNumberFromUkrlp}";
                 _logger.LogError(ex, message);
                 throw new ServiceUnavailableException(message);
             }
+
+            
+        }
+
+        private AsyncRetryPolicy GetRetryPolicy()
+        {
+            return Policy
+                .Handle<Exception>()
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(1),
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(4)
+                }, (exception, timeSpan, retryCount, context) =>
+                {
+                    _logger.LogWarning($"Error retrieving response from companies house or charity commission. Reason: {exception.Message}. Retrying in {timeSpan.Seconds} secs...attempt: {retryCount}");
+                });
         }
     }
 }
