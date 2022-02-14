2dafe4dea00672d7c2283c53b2e5c3f2a14272e2
MarkFCain <31033361+MarkFCain@users.noreply.github.com>
2022-02-14 15:40:18 +0000

    APR-2908 create account page validation code and coverage complete (#949)
    * APR-2908 create account page validation code and coverage complete
    
    * APR-2908 reworked to use fluentValidator and matching partial summary
    
    * APR-2908 snagging
    
    * APR-2908 updating all suitable pages to use new partial
    
    * APR-2908 minor format fix

---
+++ src/SFA.DAS.ApplyService.Web.UnitTests/Validators/CreateAccountViewModelValidatorTests.cs
@@ -0,0 +1,142 @@
+﻿using System.Linq;
+using NPOI.OpenXmlFormats.Spreadsheet;
+using NUnit.Framework;
+using SFA.DAS.ApplyService.Web.Validators;
+using SFA.DAS.ApplyService.Web.ViewModels;
+
+namespace SFA.DAS.ApplyService.Web.UnitTests.Validators
+{
+    [TestFixture]
+    public class CreateAccountViewModelValidatorTests
+    {
+
+        private CreateAccountViewModelValidator _validator;
+        private CreateAccountViewModel _viewModel;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _validator = new CreateAccountViewModelValidator();
+            _viewModel = new CreateAccountViewModel
+            {
+
+                GivenName = "FirstName",
+                FamilyName = "LastName",
+                Email = "name@location.com"
+            };
+        }
+
+        [Test]
+        public void Validate_Returns_No_Error_Messages_When_Mandatory_Fields_Are_Present_And_Correct()
+        {
+            var results = _validator.Validate(_viewModel);
+            Assert.IsTrue(results.IsValid);
+        }
+
+        [Test]
+        public void Validate_Returns_Appropriate_Error_When_GivenName_Missing()
+        {
+            _viewModel.GivenName = null;
+
+            var results = _validator.Validate(_viewModel);
+
+            Assert.AreEqual(1, results.Errors.Count);
+            Assert.AreEqual(nameof(_viewModel.GivenName), results.Errors[0].PropertyName);
+            Assert.AreEqual(CreateAccountViewModelValidator.FirstNameMinLengthError, results.Errors[0].ErrorMessage);
+        }
+
+        [Test]
+        public void Validate_Returns_Appropriate_Error_When_GivenName_Exceeds_MaxLength()
+        {
+            const int maxLength = 250;
+            _viewModel.GivenName = string.Concat(Enumerable.Repeat("a", maxLength + 1));
+        
+            var results = _validator.Validate(_viewModel);
+            
+            Assert.AreEqual(1, results.Errors.Count);
+            Assert.AreEqual(nameof(_viewModel.GivenName),  results.Errors[0].PropertyName);
+            Assert.AreEqual(CreateAccountViewModelValidator.FirstNameMaxLengthError, results.Errors[0].ErrorMessage);
+        }
+        
+        [Test]
+        public void Validate_Returns_Appropriate_Error_When_FamilyName_Missing()
+        {
+            _viewModel.FamilyName = null;
+        
+            var results = _validator.Validate(_viewModel);
+
+            Assert.AreEqual(1, results.Errors.Count);
+            Assert.AreEqual(nameof(_viewModel.FamilyName), results.Errors[0].PropertyName);
+            Assert.AreEqual(CreateAccountViewModelValidator.LastNameMinLengthError, results.Errors[0].ErrorMessage);
+        }
+        
+        [Test]
+        public void Validate_Returns_Appropriate_Error_When_FamilyName_Exceeds_MaxLength()
+        {
+            const int maxLength = 250;
+            _viewModel.FamilyName = string.Concat(Enumerable.Repeat("a", maxLength + 1));
+        
+            var results = _validator.Validate(_viewModel);
+            
+            Assert.AreEqual(1, results.Errors.Count);
+            Assert.AreEqual(nameof(_viewModel.FamilyName), results.Errors[0].PropertyName);
+            Assert.AreEqual(CreateAccountViewModelValidator.LastNameMaxLengthError, results.Errors[0].ErrorMessage);
+        }
+        
+        [Test]
+        public void Validate_Returns_Appropriate_Error_When_Email_Missing()
+        {
+            _viewModel.Email = null;
+        
+            var results = _validator.Validate(_viewModel);
+        
+            Assert.AreEqual(1, results.Errors.Count);
+            Assert.AreEqual(nameof(_viewModel.Email), results.Errors[0].PropertyName);
+            Assert.AreEqual(CreateAccountViewModelValidator.EmailError, results.Errors[0].ErrorMessage);
+        }
+        
+        [Test]
+        public void Validate_Returns_Appropriate_Error_When_Email_Exceeds_MaxLength()
+        {
+            const int maxLength = 256;
+            _viewModel.Email = string.Concat(Enumerable.Repeat("a", maxLength)) + "@location.com";
+        
+            var results = _validator.Validate(_viewModel);
+            
+            Assert.AreEqual(1, results.Errors.Count);
+            Assert.AreEqual(nameof(_viewModel.Email), results.Errors[0].PropertyName);
+            Assert.AreEqual(CreateAccountViewModelValidator.EmailErrorTooLong, results.Errors[0].ErrorMessage);
+        }
+
+        [TestCase("wrong-format")]
+        [TestCase("test")]
+        [TestCase("test@test")]
+        [TestCase("test@test      .com")]
+        public void Validate_Returns_Appropriate_Error_When_Email_is_Wrong_Format(string incorrectEmail)
+        {
+            _viewModel.Email = incorrectEmail;
+            var results = _validator.Validate(_viewModel);
+
+            Assert.AreEqual(1, results.Errors.Count);
+            Assert.AreEqual(nameof(_viewModel.Email), results.Errors[0].PropertyName);
+            Assert.AreEqual(ManagementHierarchyValidator.EmailErrorWrongFormat, results.Errors[0].ErrorMessage);
+        }
+
+        [TestCase("test@test.uk")]
+        [TestCase("test@test.com")]
+        [TestCase("test@test.co.uk")]
+        [TestCase("test.test@test.uk")]
+        [TestCase("test-test@test.uk")]
+        [TestCase("test.test@test.com")]
+        [TestCase("test-test@test.com")]
+        [TestCase("test.test@test.co.uk")]
+        [TestCase("test-test@test.co.uk")]
+        public void Validate_Returns_Valid_When_Email_is_Correct_Format(string correctEmail)
+        {
+            _viewModel.Email = correctEmail;
+        
+            var results = _validator.Validate(_viewModel);
+            Assert.IsTrue(results.IsValid);
+        }
+    }
+}
---
+++ src/SFA.DAS.ApplyService.Web/Validators/CreateAccountViewModelValidator.cs
@@ -0,0 +1,37 @@
+﻿using FluentValidation;
+using SFA.DAS.ApplyService.Web.ViewModels;
+
+namespace SFA.DAS.ApplyService.Web.Validators
+{
+    public class CreateAccountViewModelValidator : AbstractValidator<CreateAccountViewModel>
+    {
+        public const string FirstNameMinLengthError = "Enter your first name";
+        public const string FirstNameMaxLengthError = "Enter your first name using 250 characters or less";
+
+        public const string LastNameMinLengthError = "Enter your last name";
+        public const string LastNameMaxLengthError = "Enter your last name using 250 characters or less";
+
+        public const string ValidEmailPattern = EmailValidation.RegexPattern;
+        public const string EmailError = "Enter your email";
+        public const string EmailErrorTooLong = "Enter your email using 256 characters or less";
+
+        public const string EmailErrorWrongFormat =
+            "Enter your email address using an acceptable format (eg name@location.com)";
+
+        public CreateAccountViewModelValidator()
+        {
+            RuleFor(x => x.GivenName)
+                 .NotEmpty().WithMessage(FirstNameMinLengthError)
+                 .MaximumLength(250).WithMessage(FirstNameMaxLengthError);
+
+            RuleFor(x => x.FamilyName)
+                .NotEmpty().WithMessage(LastNameMinLengthError)
+                .MaximumLength(250).WithMessage(LastNameMaxLengthError);
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage(EmailError)
+                .MaximumLength(256).WithMessage(EmailErrorTooLong)
+                .Matches(ValidEmailPattern).WithMessage(EmailErrorWrongFormat);
+        }
+    }
+}
