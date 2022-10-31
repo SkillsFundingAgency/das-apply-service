b3027eb3ed2e168b0e57e16d732bf72cb556cdc5
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-10-31 17:18:44 +0000

    CSP-325 code refactored

--- src/SFA.DAS.ApplyService.Web/Startup.cs
+++ src/SFA.DAS.ApplyService.Web/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using FluentValidation.AspNetCore;
+﻿using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Localization;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,6 +33,9 @@ using SFA.DAS.ApplyService.Web.Validators;
 using SFA.DAS.Http;
 using SFA.DAS.Http.TokenGenerators;
 using SFA.DAS.Notifications.Api.Client;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
 
 namespace SFA.DAS.ApplyService.Web
 {
