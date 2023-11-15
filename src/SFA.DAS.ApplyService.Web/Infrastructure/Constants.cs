4dc9f6cf3f7c553feb22727f57ff7b9f0f498a3f
Deven Shah <devenshah1974@gmail.com>
2023-11-15 12:14:18 +0000

    CSP-1068 Updated links to APAR

---
+++ src/SFA.DAS.ApplyService.Web/Infrastructure/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace SFA.DAS.ApplyService.Web.Infrastructure;
+
+public static class Constants
+{
+    public static class ExternalLinks
+    {
+        public const string RemovalConsequences = "https://www.gov.uk/government/publications/removal-from-the-apprenticeship-provider-and-assessment-register/removal-from-the-apprenticeship-provider-and-assessment-register-guidance-for-providers#consequences-and-removal-from-the-register";
+
+        public const string ComplaintsProcedure = "https://www.gov.uk/government/organisations/department-for-education/about/complaints-procedure";
+
+        public const string ConditionsOfAcceptance = "https://www.gov.uk/government/publications/apprenticeship-provider-and-assessment-register-conditions-of-acceptance";
+
+        public const string WhatIsQualityApprenticeship = "https://www.instituteforapprenticeships.org/quality/what-is-a-quality-apprenticeship";
+
+        public const string DownloadRegister = "https://download.apprenticeships.education.gov.uk";
+
+        public const string ApplicationGuidance = "https://www.gov.uk/guidance/apply-to-the-apar-as-an-apprenticeship-training-provider";
+    }
+}
