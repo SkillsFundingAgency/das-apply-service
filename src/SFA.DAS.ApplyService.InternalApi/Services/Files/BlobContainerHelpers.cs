6fe268024683c89f807209ad3ca4f3ef521b889b
MarkFCain <mark.cain@digital.education.gov.uk>
2024-02-28 14:33:25 +0000

    CSP-1186 upgrade package WindowsStorage and rearrange appsettings

--- src/SFA.DAS.ApplyService.InternalApi/Services/Files/BlobContainerHelpers.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Services/Files/BlobContainerHelpers.cs
@@ -1,90 +1,89 @@
-﻿using Microsoft.Azure.Storage;
-using Microsoft.Azure.Storage.Blob;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using SFA.DAS.ApplyService.Configuration;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.ApplyService.InternalApi.Services.Files
+namespace SFA.DAS.ApplyService.InternalApi.Services.Files;
+
+[ExcludeFromCodeCoverage]
+public static class BlobContainerHelpers
 {
-    [ExcludeFromCodeCoverage]
-    public static class BlobContainerHelpers
+    public static async Task<CloudBlobContainer> GetContainer(FileStorageConfig fileStorageConfig, ContainerType containerType)
     {
-        public static async Task<CloudBlobContainer> GetContainer(FileStorageConfig fileStorageConfig, ContainerType containerType)
+        var container = default(CloudBlobContainer);
+
+        var containerName = GetContainerName(fileStorageConfig, containerType);
+
+        if (!string.IsNullOrWhiteSpace(containerName))
         {
-            var container = default(CloudBlobContainer);
-
-            var containerName = GetContainerName(fileStorageConfig, containerType);
-
-            if (!string.IsNullOrWhiteSpace(containerName))
-            {
-                var account = CloudStorageAccount.Parse(fileStorageConfig.StorageConnectionString);
-                var client = account.CreateCloudBlobClient();
-                container = client.GetContainerReference(containerName.ToLower());
-                await container.CreateIfNotExistsAsync();
-            }
-
-            return container;
+            var account = CloudStorageAccount.Parse(fileStorageConfig.StorageConnectionString);
+            var client = account.CreateCloudBlobClient();
+            container = client.GetContainerReference(containerName.ToLower());
+            await container.CreateIfNotExistsAsync();
         }
 
-        public static CloudBlobDirectory GetDirectory(this CloudBlobContainer container, Guid applicationId)
-        {
-            return container.GetApplicationDirectory(applicationId);
-        }
+        return container;
+    }
 
-        public static CloudBlobDirectory GetDirectory(this CloudBlobContainer container, Guid applicationId, int? sequenceNumber, int? sectionNumber, string pageId)
-        {
-            if(string.IsNullOrWhiteSpace(pageId))
-            {
-                throw new ArgumentNullException(nameof(pageId));
-            }
-            else if(sequenceNumber is null || sectionNumber is null)
-            {
-                return container.GetPageDirectory(applicationId, pageId);
-                
-            }
-            else
-            {
-                return container.GetPageDirectory(applicationId, sequenceNumber.Value, sectionNumber.Value, pageId);
-            }
-        }
+    public static CloudBlobDirectory GetDirectory(this CloudBlobContainer container, Guid applicationId)
+    {
+        return container.GetApplicationDirectory(applicationId);
+    }
 
-        private static CloudBlobDirectory GetApplicationDirectory(this CloudBlobContainer container, Guid applicationId)
+    public static CloudBlobDirectory GetDirectory(this CloudBlobContainer container, Guid applicationId, int? sequenceNumber, int? sectionNumber, string pageId)
+    {
+        if (string.IsNullOrWhiteSpace(pageId))
         {
-            return container.GetDirectoryReference(applicationId.ToString());;
+            throw new ArgumentNullException(nameof(pageId));
         }
-
-        private static CloudBlobDirectory GetPageDirectory(this CloudBlobContainer container, Guid applicationId, string pageId)
+        else if (sequenceNumber is null || sectionNumber is null)
         {
-            var applicationFolder = container.GetApplicationDirectory(applicationId);
-            var pageFolder = applicationFolder.GetDirectoryReference(pageId.ToLower());
-            return pageFolder;
+            return container.GetPageDirectory(applicationId, pageId);
+
         }
-
-        private static CloudBlobDirectory GetPageDirectory(this CloudBlobContainer container, Guid applicationId, int sequenceNumber, int sectionNumber, string pageId)
+        else
         {
-            var applicationFolder = container.GetApplicationDirectory(applicationId);
-            var sequenceFolder = applicationFolder.GetDirectoryReference(sequenceNumber.ToString());
-            var sectionFolder = sequenceFolder.GetDirectoryReference(sectionNumber.ToString());
-            var pageFolder = sectionFolder.GetDirectoryReference(pageId.ToLower());
-            return pageFolder;
+            return container.GetPageDirectory(applicationId, sequenceNumber.Value, sectionNumber.Value, pageId);
         }
+    }
 
-        private static string GetContainerName(FileStorageConfig fileStorageConfig, ContainerType containerType)
+    private static CloudBlobDirectory GetApplicationDirectory(this CloudBlobContainer container, Guid applicationId)
+    {
+        return container.GetDirectoryReference(applicationId.ToString()); ;
+    }
+
+    private static CloudBlobDirectory GetPageDirectory(this CloudBlobContainer container, Guid applicationId, string pageId)
+    {
+        var applicationFolder = container.GetApplicationDirectory(applicationId);
+        var pageFolder = applicationFolder.GetDirectoryReference(pageId.ToLower());
+        return pageFolder;
+    }
+
+    private static CloudBlobDirectory GetPageDirectory(this CloudBlobContainer container, Guid applicationId, int sequenceNumber, int sectionNumber, string pageId)
+    {
+        var applicationFolder = container.GetApplicationDirectory(applicationId);
+        var sequenceFolder = applicationFolder.GetDirectoryReference(sequenceNumber.ToString());
+        var sectionFolder = sequenceFolder.GetDirectoryReference(sectionNumber.ToString());
+        var pageFolder = sectionFolder.GetDirectoryReference(pageId.ToLower());
+        return pageFolder;
+    }
+
+    private static string GetContainerName(FileStorageConfig fileStorageConfig, ContainerType containerType)
+    {
+        switch (containerType)
         {
-            switch(containerType)
-            {
-                case ContainerType.Gateway:
-                    return fileStorageConfig.GatewayContainerName;
-                case ContainerType.Financial:
-                    return fileStorageConfig.FinancialContainerName;
-                case ContainerType.Assessor:
-                    return fileStorageConfig.AssessorContainerName;
-                case ContainerType.Appeals:
-                    return fileStorageConfig.AppealsContainerName;
-                default:
-                    return null;
-            }
+            case ContainerType.Gateway:
+                return fileStorageConfig.GatewayContainerName;
+            case ContainerType.Financial:
+                return fileStorageConfig.FinancialContainerName;
+            case ContainerType.Assessor:
+                return fileStorageConfig.AssessorContainerName;
+            case ContainerType.Appeals:
+                return fileStorageConfig.AppealsContainerName;
+            default:
+                return null;
         }
     }
 }
--- src/SFA.DAS.ApplyService.InternalApi/Services/Files/BlobDirectoryHelpers.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Services/Files/BlobDirectoryHelpers.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Azure.Storage.Blob;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
@@ -8,118 +8,117 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.ApplyService.InternalApi.Services.Files
+namespace SFA.DAS.ApplyService.InternalApi.Services.Files;
+
+[ExcludeFromCodeCoverage]
+public static class BlobDirectoryHelpers
 {
-    [ExcludeFromCodeCoverage]
-    public static class BlobDirectoryHelpers
+    public static async Task<IEnumerable<DownloadFileInfo>> GetFileList(this CloudBlobDirectory directory)
     {
-        public static IEnumerable<DownloadFileInfo> GetFileList(this CloudBlobDirectory directory)
+        var fileList = new List<DownloadFileInfo>();
+
+        var fileBlobs = await directory.ListBlobsSegmentedAsync(new BlobContinuationToken());
+
+        foreach (var blob in fileBlobs.Results.OfType<CloudBlob>())
         {
-            var fileList = new List<DownloadFileInfo>();
-
-            var fileBlobs = directory.ListBlobs(useFlatBlobListing: true).ToList();
-
-            foreach (var blob in fileBlobs.OfType<CloudBlob>())
+            var fileInfo = new DownloadFileInfo
             {
-                var fileInfo = new DownloadFileInfo
-                {
-                    FileName = Path.GetFileName(blob.Name),
-                    ContentType = blob.Properties.ContentType,
-                    Size = blob.Properties.Length,
-                    CreatedOn = blob.Properties.Created?.DateTime ?? System.DateTime.Now
-                };
+                FileName = Path.GetFileName(blob.Name),
+                ContentType = blob.Properties.ContentType,
+                Size = blob.Properties.Length,
+                CreatedOn = blob.Properties.Created?.DateTime ?? System.DateTime.Now
+            };
 
-                fileList.Add(fileInfo);
-            }
-
-            return fileList;
+            fileList.Add(fileInfo);
         }
 
-        public static async Task<bool> UploadFiles(this CloudBlobDirectory directory, Microsoft.AspNetCore.Http.IFormFileCollection files, ILogger<FileStorageService> logger, CancellationToken cancellationToken)
+        return fileList;
+    }
+
+    public static async Task<bool> UploadFiles(this CloudBlobDirectory directory, Microsoft.AspNetCore.Http.IFormFileCollection files, ILogger<FileStorageService> logger, CancellationToken cancellationToken)
+    {
+        bool success;
+
+        try
         {
-            bool success;
-
-            try
+            foreach (var file in files)
             {
-                foreach (var file in files)
-                {
-                    var blob = directory.GetBlockBlobReference(file.FileName);
-                    blob.Properties.ContentType = file.ContentType;
+                var blob = directory.GetBlockBlobReference(file.FileName);
+                blob.Properties.ContentType = file.ContentType;
 
-                    await blob.UploadFromStreamAsync(file.OpenReadStream(), cancellationToken);
-                }
-
-                success = true;
-            }
-            catch (Exception ex)
-            {
-                logger.LogError($"Error uploading files to directory: {directory.Uri} || Message: {ex.Message} || Stack trace: {ex.StackTrace}");
-                success = false;
+                await blob.UploadFromStreamAsync(file.OpenReadStream());
             }
 
-            return success;
+            success = true;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Error uploading files to directory: {directory.Uri} || Message: {ex.Message} || Stack trace: {ex.StackTrace}");
+            success = false;
         }
 
-        public static async Task<bool> DeleteFile(this CloudBlobDirectory directory, string fileName, CancellationToken cancellationToken)
-        {
-            var blob = directory.GetBlobReference(fileName);
+        return success;
+    }
 
-            return await blob.DeleteIfExistsAsync(cancellationToken);
+    public static async Task<bool> DeleteFile(this CloudBlobDirectory directory, string fileName, CancellationToken cancellationToken)
+    {
+        var blob = directory.GetBlobReference(fileName);
+
+        return await blob.DeleteIfExistsAsync();
+    }
+
+    public static async Task<DownloadFile> DownloadFile(this CloudBlobDirectory directory, string fileName, CancellationToken cancellationToken)
+    {
+        var blob = directory.GetBlobReference(fileName);
+
+        return await DownloadFileFromBlob(blob, cancellationToken);
+    }
+
+    public static async Task<bool> DeleteDirectory(this CloudBlobDirectory directory, CancellationToken cancellationToken)
+    {
+        var fileBlobs = await directory.ListBlobsSegmentedAsync(new BlobContinuationToken());
+
+        var deleteResults = new List<bool>(fileBlobs.Results.Count());
+
+        foreach (var blob in fileBlobs.Results.OfType<CloudBlob>())
+        {
+            var result = await blob.DeleteIfExistsAsync();
+            deleteResults.Add(result);
         }
 
-        public static async Task<DownloadFile> DownloadFile(this CloudBlobDirectory directory, string fileName, CancellationToken cancellationToken)
-        {
-            var blob = directory.GetBlobReference(fileName);
+        return deleteResults.All(r => r is true);
 
-            return await DownloadFileFromBlob(blob, cancellationToken);
+    }
+
+    public static async Task<IEnumerable<DownloadFile>> DownloadDirectoryFiles(this CloudBlobDirectory directory, CancellationToken cancellationToken)
+    {
+        var fileBlobs = await directory.ListBlobsSegmentedAsync(new BlobContinuationToken());
+
+        var downloadFiles = new List<DownloadFile>(fileBlobs.Results.Count());
+
+        foreach (var blob in fileBlobs.Results.OfType<CloudBlob>())
+        {
+            var downloadFile = await DownloadFileFromBlob(blob, cancellationToken);
+            downloadFiles.Add(downloadFile);
         }
 
-        public static async Task<bool> DeleteDirectory(this CloudBlobDirectory directory, CancellationToken cancellationToken)
+        return downloadFiles;
+    }
+
+    private static async Task<DownloadFile> DownloadFileFromBlob(CloudBlob blob, CancellationToken cancellationToken)
+    {
+        if (blob.ExistsAsync().Result)
         {
-            var fileBlobs = directory.ListBlobs(useFlatBlobListing: true).ToList();
+            var blobStream = new MemoryStream();
 
-            var deleteResults = new List<bool>(fileBlobs.Count);
+            await blob.DownloadToStreamAsync(blobStream, null, new BlobRequestOptions() { DisableContentMD5Validation = true }, null, cancellationToken);
+            blobStream.Position = 0;
 
-            foreach (var blob in fileBlobs.OfType<CloudBlob>())
-            {
-                var result = await blob.DeleteIfExistsAsync(cancellationToken);
-                deleteResults.Add(result);
-            }
-
-            return deleteResults.All(r => r is true);
-
+            return new DownloadFile { FileName = Path.GetFileName(blob.Name), ContentType = blob.Properties.ContentType, Stream = blobStream };
         }
-
-        public static async Task<IEnumerable<DownloadFile>> DownloadDirectoryFiles(this CloudBlobDirectory directory, CancellationToken cancellationToken)
+        else
         {
-            var fileBlobs = directory.ListBlobs(useFlatBlobListing: true).ToList();
-
-            var downloadFiles = new List<DownloadFile>(fileBlobs.Count);
-
-            foreach (var blob in fileBlobs.OfType<CloudBlob>())
-            {
-                var downloadFile = await DownloadFileFromBlob(blob, cancellationToken);
-                downloadFiles.Add(downloadFile);
-            }
-
-            return downloadFiles;
-        }
-
-        private static async Task<DownloadFile> DownloadFileFromBlob(CloudBlob blob, CancellationToken cancellationToken)
-        {
-            if (blob.Exists())
-            {
-                var blobStream = new MemoryStream();
-
-                await blob.DownloadToStreamAsync(blobStream, null, new BlobRequestOptions() { DisableContentMD5Validation = true }, null, cancellationToken);
-                blobStream.Position = 0;
-
-                return new DownloadFile { FileName = Path.GetFileName(blob.Name), ContentType = blob.Properties.ContentType, Stream = blobStream };
-            }
-            else
-            {
-                return default(DownloadFile);
-            }
+            return default(DownloadFile);
         }
     }
 }
