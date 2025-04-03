1bc99acb1f835a25bcfe00bbe03adf697b494c38
Peter Williams <peter.david.williams.1991@gmail.com>
2025-04-03 15:19:05 +0100

    CSP-1973-1974 - Removed nlog references

--- src/SFA.DAS.ApplyService.InternalApi/Program.cs
+++ src/SFA.DAS.ApplyService.InternalApi/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using NLog.Web;
 
 namespace SFA.DAS.ApplyService.InternalApi
 {
@@ -9,23 +7,11 @@ namespace SFA.DAS.ApplyService.InternalApi
     {
         public static void Main(string[] args)
         {
-            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
-            try
-            {
-                logger.Info("Starting up Apply API host");
-                CreateWebHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                //NLog: catch setup errors
-                logger.Error(ex, "Stopped program because of exception");
-                throw;
-            }
+
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseNLog();
+                .UseStartup<Startup>();
     }
 }
