f52a80910fbf076ec739675e69f4d48647c17b2d
Deven Shah <devenshah1974@gmail.com>
2025-11-06 10:47:22 +0000

    CSP-2288 Deleted admin service package

---
+++ src/SFA.DAS.ApplyService.Web/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.ApplyService.Web.Extensions;
+
+[ExcludeFromCodeCoverage]
+public static class DateTimeExtensions
+{
+    public static string ToSfaShortDateString(this DateTime time)
+    {
+        return time.ToString("dd MMMM yyyy");
+    }
+
+    public static string ToSfaShortDateString(this DateTime? time)
+    {
+        return time?.ToString("dd MMMM yyyy");
+    }
+}
